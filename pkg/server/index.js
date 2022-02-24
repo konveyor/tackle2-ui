54efbc2a2b280ee07038d416870e7200d6b0ef79
Gilles Dubreuil <gilles@redhat.com>
2022-02-24 09:27:34 +0100

    consolidate server file

--- pkg/server/index.js
+++ pkg/server/index.js
@@ -2,12 +2,43 @@ const express = require("express");
 const path = require("path");
 const app = express(),
   bodyParser = require("body-parser");
-
-const setupProxy = require("./setupProxy");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const port = 8080;
 
-setupProxy(app);
+app.use(
+  "/auth",
+  createProxyMiddleware({
+    target: process.env.SSO_SERVER_URL || "http://localhost:9001",
+    changeOrigin: true,
+    logLevel: process.env.DEBUG ? "debug" : "info",
+  })
+);
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target:
+      process.env.APPLICATION_INVENTORY_API_URL || "http://localhost:9002",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "",
+    },
+    logLevel: process.env.DEBUG ? "debug" : "info",
+  })
+);
+
+app.use(
+  "/api/pathfinder",
+  createProxyMiddleware({
+    target: process.env.PATHFINDER_API_URL || "http://localhost:9003",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api/pathfinder": "/pathfinder",
+    },
+    logLevel: process.env.DEBUG ? "debug" : "info",
+  })
+);
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../client/dist")));
