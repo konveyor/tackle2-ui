4f568c7d6c8433cc5bf940ee8abbec51fa730e5d
Gilles Dubreuil <gilles@redhat.com>
2022-03-06 17:49:33 +0100

    Cleansing set-targets

--- pkg/client/src/app/pages/applications/analysis-wizard/set-targets.tsx
+++ pkg/client/src/app/pages/applications/analysis-wizard/set-targets.tsx
@@ -9,25 +9,18 @@ import {
 } from "@patternfly/react-core";
 
 import { SelectCardGallery } from "./components/select-card-gallery";
-import { FormState, UseFormSetValue } from "react-hook-form";
+import { UseFormGetValues, UseFormSetValue } from "react-hook-form";
 import { IFormValues } from "./analysis-wizard";
 
 interface IAnalysisMode {
+  getValues: UseFormGetValues<IFormValues>;
   setValue: UseFormSetValue<IFormValues>;
 }
 
 export const SetTargets: React.FunctionComponent<IAnalysisMode> = ({
+  getValues,
   setValue,
 }) => {
-  const [selectedTargets, setSelectedTargets] = React.useState<string[]>([]);
-  const [dirty, setDirty] = React.useState(false);
-
-  const onSelectedTargetsChange = (values: string[]) => {
-    setDirty(true);
-    setSelectedTargets(values);
-    setValue("targets", values);
-  };
-
   return (
     <Stack hasGutter>
       <StackItem>
@@ -42,8 +35,10 @@ export const SetTargets: React.FunctionComponent<IAnalysisMode> = ({
       </StackItem>
       <StackItem>
         <SelectCardGallery
-          value={selectedTargets}
-          onChange={onSelectedTargetsChange}
+          value={getValues("targets")}
+          onChange={(value) => {
+            setValue("targets", value);
+          }}
         />
       </StackItem>
     </Stack>
