b45525d6e58d440b39227dc107e586fa8425603e
Ian Bolton <ibolton@redhat.com>
2022-03-08 15:19:12 -0500

    pr comments

--- pkg/client/src/app/pages/proxies/proxies-validation-schema.ts
+++ pkg/client/src/app/pages/proxies/proxies-validation-schema.ts
@@ -43,34 +43,31 @@ export default function validationSchema(
   };
 
   return yup.object().shape({
+    //http
     [HTTP_HOST]: yup.lazy(() =>
-      mandatoryFields[HTTPS_HOST]
-        ? yup.string().required(REQUIRED_MESSAGE)
-        : yup.string()
-    ),
-    [HTTPS_HOST]: yup.lazy(() =>
-      mandatoryFields[HTTPS_HOST]
+      mandatoryFields[HTTP_HOST]
         ? yup.string().required(REQUIRED_MESSAGE)
         : yup.string()
     ),
     [HTTP_PORT]: yup.lazy(() =>
-      mandatoryFields.httpPort
-        ? yup.string().required(REQUIRED_MESSAGE)
-        : yup.string()
-    ),
-    [HTTPS_PORT]: yup.lazy(() =>
-      mandatoryFields.httpPort
-        ? yup.string().required(REQUIRED_MESSAGE)
-        : yup.string()
-    ),
-    [HTTPS_HOST]: yup.lazy(() =>
-      mandatoryFields[HTTPS_HOST]
+      mandatoryFields[HTTP_PORT]
         ? yup.string().required(REQUIRED_MESSAGE)
         : yup.string()
     ),
     ...(mandatoryFields[HTTP_IDENTITY] && {
       [HTTP_IDENTITY]: createHttpIdentitySchema(),
     }),
+    //https
+    [HTTPS_HOST]: yup.lazy(() =>
+      mandatoryFields[HTTPS_HOST]
+        ? yup.string().required(REQUIRED_MESSAGE)
+        : yup.string()
+    ),
+    [HTTPS_PORT]: yup.lazy(() =>
+      mandatoryFields[HTTPS_PORT]
+        ? yup.string().required(REQUIRED_MESSAGE)
+        : yup.string()
+    ),
     ...(mandatoryFields[HTTPS_IDENTITY] && {
       [HTTPS_IDENTITY]: createHttpsIdentitySchema(),
     }),
