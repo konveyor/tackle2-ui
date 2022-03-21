571aca7ba741eadfed7b95fcb0d9116c8afa9565
Ian Bolton <ibolton@redhat.com>
2022-03-21 17:06:04 -0400

    Fix proxy save button disabled state when value changes  (#48)
    * fix proxy save button state when value changes
    
    * cleanup

--- pkg/client/src/app/queries/proxies.ts
+++ pkg/client/src/app/queries/proxies.ts
@@ -3,9 +3,7 @@ import { AxiosError } from "axios";
 import { createAsyncAction, getType } from "typesafe-actions";
 import { getProxies, PROXIES, updateProxy } from "@app/api/rest";
 import { Proxy } from "@app/api/models";
-import { useMutation, UseMutationResult, useQuery } from "react-query";
-import { ProxyFormValues } from "@app/pages/proxies/proxy-form";
-import { APIClient } from "@app/axios-config";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 export const {
   request: fetchRequest,
@@ -27,7 +25,7 @@ export const useFetchProxies = (
   defaultIsFetching: boolean = false
 ): IFetchState => {
   const [proxies, setProxies] = useState<Array<Proxy>>([]);
-  const { isLoading, isError, data, error } = useQuery("repoData", () =>
+  const { isLoading, refetch, isError, data, error } = useQuery("proxies", () =>
     getProxies()
       .then((res) => res)
       .then(({ data }) => {
@@ -53,14 +51,17 @@ export interface IMutateState {
 
 export const useUpdateProxyMutation = (onSuccess: any): IMutateState => {
   const [putResult, setPutResult] = useState<any>(null);
+  const queryClient = useQueryClient();
 
   const { isLoading, mutate, error } = useMutation(updateProxy, {
     onSuccess: (res) => {
       onSuccess();
       setPutResult(res);
+      queryClient.invalidateQueries("proxies");
     },
     onError: (err) => {
       setPutResult(err);
+      queryClient.invalidateQueries("proxies");
     },
   });
   return {
