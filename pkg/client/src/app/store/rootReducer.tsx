3f348e0e89749174a472066c88cc5ae5796451a2
Gilles Dubreuil <gilles@redhat.com>
2022-04-12 15:43:04 +0200

    Remove unknownTags component for tags count (#88)

--- pkg/client/src/app/store/rootReducer.tsx
+++ pkg/client/src/app/store/rootReducer.tsx
@@ -3,7 +3,6 @@ import { StateType } from "typesafe-actions";
 
 import { notificationsReducer } from "@redhat-cloud-services/frontend-components-notifications/redux";
 import { confirmDialogStateKey, confirmDialogReducer } from "./confirmDialog";
-import { unknownTagsStateKey, unknownTagsReducer } from "./unknownTags";
 import { bulkCopyStateKey, bulkCopyReducer } from "./bulkCopy";
 
 export type RootState = StateType<typeof rootReducer>;
@@ -11,6 +10,5 @@ export type RootState = StateType<typeof rootReducer>;
 export const rootReducer = combineReducers({
   notifications: notificationsReducer,
   [confirmDialogStateKey]: confirmDialogReducer,
-  [unknownTagsStateKey]: unknownTagsReducer,
   [bulkCopyStateKey]: bulkCopyReducer,
 });
