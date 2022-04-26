bb2b74093725bfdaaf263314f28aa5fd91183a9c
Gilles Dubreuil <gilles@redhat.com>
2022-04-26 17:31:53 +0200

    Analysis Wizard packages validation (#131)
    * Analysis Wizard packages validation
    
    * Remove unused
    
    * Change validation message

--- pkg/client/src/app/pages/applications/analysis-wizard/set-scope.tsx
+++ pkg/client/src/app/pages/applications/analysis-wizard/set-scope.tsx
@@ -2,29 +2,44 @@ import * as React from "react";
 import {
   Button,
   Form,
+  FormGroup,
   InputGroup,
   Radio,
   Switch,
   Text,
-  TextArea,
+  TextInput,
   Title,
 } from "@patternfly/react-core";
 import { useFormContext } from "react-hook-form";
 import DelIcon from "@patternfly/react-icons/dist/esm/icons/error-circle-o-icon";
 import spacing from "@patternfly/react-styles/css/utilities/Spacing/spacing";
+import { useForm } from "react-hook-form";
+import { getValidatedFromError } from "@app/utils/utils";
 
 import "./wizard.css";
 
 export const SetScope: React.FunctionComponent = () => {
   const { getValues, setValue } = useFormContext();
+  const {
+    register,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
 
   const withKnown: string = getValues("withKnown");
   const includedPackages: string[] = getValues("includedPackages");
   const excludedPackages: string[] = getValues("excludedPackages");
 
   const [excludedSwitch, setExcludedSwitch] = React.useState(false);
-  const [packagesToInclude, setPackagesToInclude] = React.useState("");
-  const [packagesToExclude, setPackagesToExclude] = React.useState("");
+  const [packageToInclude, setPackageToInclude] = React.useState("");
+  const [packageToExclude, setPackageToExclude] = React.useState("");
+
+  const onChangePackageToInclude = (value: string) => {
+    setPackageToInclude(value);
+  };
+
+  const onChangePackageToExclude = (value: string) => {
+    setPackageToExclude(value);
+  };
 
   return (
     <Form>
@@ -35,8 +50,8 @@ export const SetScope: React.FunctionComponent = () => {
         Select the scope of dependencies you want to include in the analysis.
       </Text>
       <Radio
-        id="deps-only"
-        name="deps-only"
+        id="app"
+        name="app"
         isChecked={withKnown === "app"}
         onChange={() => {
           setValue("withKnown", "app");
@@ -45,8 +60,8 @@ export const SetScope: React.FunctionComponent = () => {
         className={spacing.mbXs}
       />
       <Radio
-        id="deps-all"
-        name="deps-all"
+        id="oss"
+        name="oss"
         isChecked={withKnown === "app,oss"}
         onChange={() => {
           setValue("withKnown", "app,oss");
@@ -55,8 +70,8 @@ export const SetScope: React.FunctionComponent = () => {
         className={spacing.mbXs}
       />
       <Radio
-        id="deps-select"
-        name="deps-select"
+        id="select"
+        name="select"
         isChecked={withKnown === "app,oss,select"}
         onChange={() => {
           setValue("withKnown", "app,oss,select");
@@ -64,28 +79,46 @@ export const SetScope: React.FunctionComponent = () => {
         label="Select the list of packages to be analyzed manually"
         className={spacing.mbXs}
       />
-      {withKnown === "depsSelect" && (
+      {withKnown.includes("select") && (
         <>
-          <InputGroup className={`${spacing.mtMd} ${spacing.plLg}`}>
-            <TextArea
-              name="included-packages"
-              id="included-packages"
-              aria-label="Packages to include"
-              value={packagesToInclude}
-              onChange={(value) => setPackagesToInclude(value)}
-            />
-            <Button
-              id="add-to-included-packages-list"
-              variant="control"
-              onClick={() => {
-                const list = packagesToInclude.split(",");
-                setValue("includedPackages", [...new Set(list)]);
-                setPackagesToInclude("");
-              }}
-            >
-              Add
-            </Button>
-          </InputGroup>
+          <FormGroup
+            fieldId="packageToInclude"
+            className={`${spacing.mtMd} ${spacing.plLg}`}
+            validated={getValidatedFromError(errors.packageToInclude)}
+            helperTextInvalid={errors?.packageToInclude?.message as string}
+          >
+            <InputGroup>
+              <TextInput
+                aria-label="Packages to include"
+                {...register("packageToInclude", {
+                  pattern: {
+                    value: /^[a-z]+(.[a-z0-9]+)*$/,
+                    message:
+                      "The package name should be a valid Java package name",
+                  },
+                })}
+                onChange={onChangePackageToInclude}
+                validated={getValidatedFromError(errors.packageToInclude)}
+                value={packageToInclude}
+              />
+              <Button
+                id="add-package"
+                variant="control"
+                isDisabled={
+                  getValidatedFromError(errors.packageToInclude) === "error"
+                }
+                onClick={() => {
+                  setValue("includedPackages", [
+                    ...includedPackages,
+                    packageToInclude,
+                  ]);
+                  setPackageToInclude("");
+                }}
+              >
+                Add
+              </Button>
+            </InputGroup>
+          </FormGroup>
           {includedPackages && (
             <div className={spacing.plLg}>
               {includedPackages.map(
@@ -120,26 +153,44 @@ export const SetScope: React.FunctionComponent = () => {
           onChange={() => setExcludedSwitch(!excludedSwitch)}
         />
         {excludedSwitch && (
-          <InputGroup className={`${spacing.mtMd} ${spacing.plLg}`}>
-            <TextArea
-              name="excluded-packages"
-              id="excluded-packages"
-              aria-label="Packages to exclude"
-              value={packagesToExclude}
-              onChange={(value) => setPackagesToExclude(value)}
-            />
-            <Button
-              id="add-to-excluded-packages-list"
-              variant="control"
-              onClick={() => {
-                const list = packagesToExclude.split(",");
-                setValue("excludedPackages", [...new Set(list)]);
-                setPackagesToExclude("");
-              }}
-            >
-              Add
-            </Button>
-          </InputGroup>
+          <FormGroup
+            fieldId="packageToExclude"
+            className={`${spacing.mtMd} ${spacing.plLg}`}
+            validated={getValidatedFromError(errors.packageToExclude)}
+            helperTextInvalid={errors?.packageToExclude?.message as string}
+          >
+            <InputGroup>
+              <TextInput
+                aria-label="Packages to exclude"
+                {...register("packageToExclude", {
+                  pattern: {
+                    value: /^[a-z]+(.[a-z0-9]+)*$/,
+                    message:
+                      "The package name should be a valid Java package name",
+                  },
+                })}
+                onChange={onChangePackageToExclude}
+                validated={getValidatedFromError(errors.packageToExclude)}
+                value={packageToExclude}
+              />
+              <Button
+                id="add-to-excluded-packages-list"
+                variant="control"
+                isDisabled={
+                  getValidatedFromError(errors.packageToExclude) === "error"
+                }
+                onClick={() => {
+                  setValue("excludedPackages", [
+                    ...excludedPackages,
+                    packageToExclude,
+                  ]);
+                  setPackageToExclude("");
+                }}
+              >
+                Add
+              </Button>
+            </InputGroup>
+          </FormGroup>
         )}
         {excludedPackages && (
           <div className={spacing.plLg}>
