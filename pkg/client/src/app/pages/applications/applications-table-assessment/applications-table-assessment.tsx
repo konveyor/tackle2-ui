f5643a2b0957ad67cfc0b7f4524d354a2af8ca2f
Ian Bolton <ibolton@redhat.com>
2022-05-11 10:23:14 -0400

    Missing is auth check (#192)

--- pkg/client/src/app/pages/applications/applications-table-assessment/applications-table-assessment.tsx
+++ pkg/client/src/app/pages/applications/applications-table-assessment/applications-table-assessment.tsx
@@ -80,6 +80,7 @@ import {
 import { FilterToolbar } from "@app/shared/components/FilterToolbar/FilterToolbar";
 import { useFetchTags } from "@app/queries/tags";
 import { useFetchReviews } from "@app/queries/reviews";
+import { isAuthRequired } from "@app/Constants";
 
 const ENTITY_FIELD = "entity";
 
@@ -436,7 +437,7 @@ export const ApplicationsTable: React.FC = () => {
     }
     const userScopes: string[] = token?.scope.split(" "),
       access = userScopes && checkAccess(userScopes, writeScopes);
-    if (access) {
+    if (access || !isAuthRequired) {
       actions.push(
         {
           title: t("actions.manageDependencies"),
