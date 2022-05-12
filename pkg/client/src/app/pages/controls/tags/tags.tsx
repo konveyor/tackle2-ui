f0a22b8b95569d834c0a6e2fab196f21319e7b03
Jeff Ortel <jortel@redhat.com>
2022-05-12 16:01:19 -0500

    Revert "Add rbac rules for tag page (#200) (#204)"
    This reverts commit 2152c4e67a888e7a1c5cd506228425944a1b4f26.

--- pkg/client/src/app/pages/controls/tags/tags.tsx
+++ pkg/client/src/app/pages/controls/tags/tags.tsx
@@ -50,7 +50,6 @@ import {
 import { useFilterState } from "@app/shared/hooks/useFilterState";
 import { useSortState } from "@app/shared/hooks/useSortState";
 import { DEFAULT_COLOR_LABELS } from "@app/Constants";
-import { RBAC, RBAC_TYPE, writeScopes } from "@app/rbac";
 
 const ENTITY_FIELD = "entity";
 
@@ -412,28 +411,26 @@ export const Tags: React.FC = () => {
           }
           toolbarActions={
             <ToolbarGroup variant="button-group">
-              <RBAC allowedPermissions={writeScopes} rbacType={RBAC_TYPE.Scope}>
-                <ToolbarItem>
-                  <Button
-                    type="button"
-                    aria-label="create-tag"
-                    variant={ButtonVariant.primary}
-                    onClick={handleOnOpenCreateNewTagModal}
-                  >
-                    {t("actions.createTag")}
-                  </Button>
-                </ToolbarItem>
-                <ToolbarItem>
-                  <Button
-                    type="button"
-                    aria-label="create-tag-type"
-                    variant={ButtonVariant.secondary}
-                    onClick={handleOnOpenCreateNewTagTypeModal}
-                  >
-                    {t("actions.createTagType")}
-                  </Button>
-                </ToolbarItem>
-              </RBAC>
+              <ToolbarItem>
+                <Button
+                  type="button"
+                  aria-label="create-tag"
+                  variant={ButtonVariant.primary}
+                  onClick={handleOnOpenCreateNewTagModal}
+                >
+                  {t("actions.createTag")}
+                </Button>
+              </ToolbarItem>
+              <ToolbarItem>
+                <Button
+                  type="button"
+                  aria-label="create-tag-type"
+                  variant={ButtonVariant.secondary}
+                  onClick={handleOnOpenCreateNewTagTypeModal}
+                >
+                  {t("actions.createTagType")}
+                </Button>
+              </ToolbarItem>
             </ToolbarGroup>
           }
           noDataState={
