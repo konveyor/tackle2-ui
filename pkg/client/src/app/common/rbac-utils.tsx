cb855bfc29becddcb7a093e28cd93d9c6ff02145
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-06-07 13:33:23 -0400

    Fix AUTH_REQUIRED flag not affecting direct calls to checkAccess function (#283)

--- pkg/client/src/app/common/rbac-utils.tsx
+++ pkg/client/src/app/common/rbac-utils.tsx
@@ -1,7 +1,10 @@
+import { isAuthRequired } from "@app/Constants";
+
 export const checkAccess = (
   userPermissions: string[],
   allowedPermissions: string[]
 ) => {
+  if (!isAuthRequired) return true;
   const access = userPermissions.some((userPermission) =>
     allowedPermissions.includes(userPermission)
   );
