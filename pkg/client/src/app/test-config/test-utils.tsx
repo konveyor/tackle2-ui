47bb4c726f1109e8c7001bf5f89f294ccf3bccec
Ian Bolton <ibolton@redhat.com>
2022-06-23 12:52:22 -0400

    Add unit test support for react query driven components (#301)
    * Add unit test to check proxy initial load
    
    * Add mock for i18n
    
    * update ss
    
    * Add test case to cover TACKLE-678

---
+++ pkg/client/src/app/test-config/test-utils.tsx
@@ -0,0 +1,22 @@
+import React, { FC, ReactElement } from "react";
+
+import { render, RenderOptions } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
