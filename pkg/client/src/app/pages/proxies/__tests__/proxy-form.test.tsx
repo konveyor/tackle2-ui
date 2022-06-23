7f4bdd0025ce36ef5d72ac7841b9ce86ea6b0d64
Ian Bolton <ibolton@redhat.com>
2022-06-23 18:07:22 -0400

    wip

--- pkg/client/src/app/pages/proxies/__tests__/proxy-form.test.tsx
+++ pkg/client/src/app/pages/proxies/__tests__/proxy-form.test.tsx
@@ -13,13 +13,17 @@ import MockAdapter from "axios-mock-adapter";
 import { IDENTITIES, PROXIES } from "@app/api/rest";
 import axios from "axios";
 import { Proxy, Identity } from "@app/api/models";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("react-i18next");
-const identitiesData: Identity[] = [{ id: 0, name: "cred1" }];
+const identitiesData: Identity[] = [
+  { id: 0, name: "proxy-cred", kind: "proxy" },
+  { id: 1, name: "cred2", kind: "maven" },
+  { id: 2, name: "cred3", kind: "source" },
+];
 new MockAdapter(axios).onGet(`${IDENTITIES}`).reply(200, identitiesData);
 
 const proxiesData = [
-  // const proxiesData: Proxy[] = [
   {
     host: "",
     kind: "http",
@@ -81,5 +85,66 @@ describe("Component: proxy-form", () => {
     await waitFor(() =>
       expect(screen.queryByTestId("http-host-input")).not.toBeInTheDocument()
     );
+  });
+
+  it("Select http proxy identity", async () => {
+    render(<Proxies />);
+    await waitFor(() => screen.getByTestId("http-proxy-switch"), {
+      timeout: 3000,
+    });
+
+    fireEvent.click(screen.getByTestId("http-proxy-switch"));
+    await waitFor(() =>
+      expect(
+        screen.queryByTestId("http-proxy-identity-switch")
+      ).toBeInTheDocument()
+    );
+    fireEvent.click(screen.getByTestId("http-proxy-identity-switch"));
+    fireEvent.click(
+      screen.getByRole("button", {
+        name: /Options menu/i,
+      })
+    );
+    // fireEvent.click(
+    //   screen.getByRole("listbox", {
+    //     name: /httpIdentity/i,
+    //   })
+    // );
+    const selectBox = screen.getByRole("listbox", {
+      name: /httpIdentity/i,
+    });
+
+    userEvent.selectOptions(
+      selectBox,
+      screen.getByRole("option", {
+        name: /proxy-cred/i,
+      })
+    );
+    const option = screen.getByRole("option", {
+      name: "proxy-cred",
+    }) as HTMLInputElement;
+
+    expect(option).selected.toBe(true);
+
+    // await userEvent.click(screen.getByRole("button", { name: /click me!/i }));
+    // userEvent.selectOptions(screen.getByRole("listbox"), ["proxy-cred"]);
+    // userEvent.selectOptions(screen.getByText("proxy-cred"), "proxy-cred");
+    // const title = screen.getByText("proxy-cred") as HTMLInputElement;
+
+    // expect(screen.getByRole("option", { name: "proxy-cred" }).selected).toBe(
+    //   true
+    // );
+
+    // expect(screen.getByRole("option", { name: "proxy-cred" }).selected).toBe(true);
+
+    // userEvent.selectOptions(
+    //   // Find the select element
+    //   screen.getByRole('combobox'),
+    //   // Find and select the Ireland option
+    //   screen.getByRole('option', {name: 'Ireland'}),
+    // )
+    // await waitFor(() =>
+    //   expect(screen.queryByTestId("http-host-input")).not.toBeInTheDocument()
+    // );
   });
 });
