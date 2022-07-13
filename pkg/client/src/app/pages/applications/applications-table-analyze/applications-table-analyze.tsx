f4724669ea4584ca6dcc6b6ba815219e49c11397
Gilles Dubreuil <gilles@redhat.com>
2022-07-13 15:33:13 +0200

    Task cancelation (#307)
    Co-authored-by: Ian Bolton <ibolton@redhat.com>

--- pkg/client/src/app/pages/applications/applications-table-analyze/applications-table-analyze.tsx
+++ pkg/client/src/app/pages/applications/applications-table-analyze/applications-table-analyze.tsx
@@ -50,7 +50,11 @@ import { ApplicationAnalysisStatus } from "../components/application-analysis-st
 import { FilterToolbar } from "@app/shared/components/FilterToolbar";
 import { AnalysisWizard } from "../analysis-wizard/analysis-wizard";
 import { ApplicationIdentityForm } from "../components/application-identity-form/application-identity-form";
-import { useDeleteTaskMutation, useFetchTasks } from "@app/queries/tasks";
+import {
+  useDeleteTaskMutation,
+  useCancelTaskMutation,
+  useFetchTasks,
+} from "@app/queries/tasks";
 import {
   applicationsWriteScopes,
   importsWriteScopes,
@@ -116,17 +120,17 @@ export const ApplicationsTableAnalyze: React.FC = () => {
 
   const { tasks } = useFetchTasks();
 
-  const completedDeleteTask = () => {
-    dispatch(alertActions.addInfo("Task", "Deleted"));
+  const completedCancelTask = () => {
+    dispatch(alertActions.addInfo("Task", "Canceled"));
   };
 
-  const failedDeleteTask = () => {
-    dispatch(alertActions.addDanger("Task", "Deletion failed."));
+  const failedCancelTask = () => {
+    dispatch(alertActions.addDanger("Task", "Cancelation failed."));
   };
 
-  const { mutate: deleteTask } = useDeleteTaskMutation(
-    completedDeleteTask,
-    failedDeleteTask
+  const { mutate: cancelTask } = useCancelTaskMutation(
+    completedCancelTask,
+    failedCancelTask
   );
 
   const getTask = (application: Application) =>
@@ -411,7 +415,7 @@ export const ApplicationsTableAnalyze: React.FC = () => {
 
   const cancelAnalysis = (row: Application) => {
     const task = tasks.find((task) => task.application?.id === row.id);
-    if (task?.id) deleteTask(task.id);
+    if (task?.id) cancelTask(task.id);
   };
 
   const handleOnApplicationIdentityUpdated = () => {
