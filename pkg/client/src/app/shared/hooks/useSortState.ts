c337739e1938b8879ff84407e7c18ef80cddc956
Ian Bolton <ibolton@redhat.com>
2022-08-16 10:07:05 -0400

    TACKLE-680: Fix locale sort support & numeric sort support (#351)
    * Fix locale sort support & numeric sort support
    
    * cleanup

--- pkg/client/src/app/shared/hooks/useSortState.ts
+++ pkg/client/src/app/shared/hooks/useSortState.ts
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { ISortBy, SortByDirection } from "@patternfly/react-table";
+import keycloak from "@app/keycloak";
+import i18n from "@app/i18n";
+import { isValidDateValue } from "@testing-library/user-event/dist/utils";
 
 export interface ISortStateHook<T> {
   sortBy: ISortBy;
@@ -31,8 +34,26 @@ export const useSortState = <T>(
       const { index, direction } = sortBy;
       const aValue = getSortValues(a)[index || 0];
       const bValue = getSortValues(b)[index || 0];
-      if (aValue < bValue) return direction === "asc" ? -1 : 1;
-      if (aValue > bValue) return direction === "asc" ? 1 : -1;
+      if (typeof aValue === "string" && typeof bValue === "string") {
+        const aSortResult = aValue.localeCompare(bValue, i18n.language, {
+          sensitivity: "base",
+        });
+        const bSortResult = bValue.localeCompare(aValue, i18n.language, {
+          sensitivity: "base",
+        });
+        if (direction === "asc") {
+          return aSortResult;
+        } else {
+          return bSortResult;
+        }
+      } else if (typeof aValue === "number" && typeof bValue === "number") {
+        if (direction === "asc") return aValue - bValue;
+        else return bValue - aValue;
+      } else {
+        if (aValue > bValue) return direction === "asc" ? -1 : 1;
+        if (aValue < bValue) return direction === "asc" ? -1 : 1;
+      }
+
       return 0;
     });
   }
