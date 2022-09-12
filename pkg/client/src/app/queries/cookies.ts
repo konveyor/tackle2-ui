1c31a3de76508c5d3ba2989557fd5e6441a9b3ff
Ian Bolton <ibolton@redhat.com>
2022-09-12 17:01:18 -0400

    Cookie management improvements (#397)
    * Refine cookie refresh logic
    
    * Redirect to app login/ regeneration when token is expired
    
    * Change token refresh time to an hour
    
    * Cleanup
    
    * Redirect home which will pick up keycloak auth flow
    
    * PR updates

--- pkg/client/src/app/queries/cookies.ts
+++ pkg/client/src/app/queries/cookies.ts
@@ -1,52 +1,27 @@
-import { useQuery } from "react-query";
-import keycloak from "@app/keycloak";
+type CookieNames = "keycloak_cookie";
 
-export const CookieQueryKey = "cookies";
+export const deleteCookie = (name: CookieNames) => {
+  document.cookie = `${name} =; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+};
 
-export const useFetchCookie = (token) => {
-  const deleteCookie = (name: string) => {
-    document.cookie = `${name} =; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
-  };
+export const setCookie = (
+  name: CookieNames,
+  value: string,
+  expDays: number
+) => {
+  let date = new Date();
+  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = `${name} = ${value}; ${expires}; path=/`;
+};
 
-  const setCookie = (cName: string, cValue: string, expDays: number) => {
-    let date = new Date();
-    date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = `${cName} = ${cValue}; ${expires}; path=/`;
-  };
-
-  const getCookie = (token: string) => {
-    let cookieArr = document.cookie.split(";");
-    for (let i = 0; i < cookieArr.length; i++) {
-      let cookiePair = cookieArr[i].split("=");
-      if (token == cookiePair[0].trim()) {
-        return decodeURIComponent(cookiePair[1]);
-      }
+export const getCookie = (name: CookieNames) => {
+  let cookieArr = document.cookie.split(";");
+  for (let i = 0; i < cookieArr.length; i++) {
+    let cookiePair = cookieArr[i].split("=");
+    if (name === cookiePair[0].trim()) {
+      return decodeURIComponent(cookiePair[1]);
     }
-    return null;
-  };
-
-  useQuery(
-    [CookieQueryKey, token],
-    () => {
-      if (token) {
-        deleteCookie("proxyToken");
-        let token = getCookie("proxyToken");
-        if (token !== "" && token !== null) {
-        } else {
-          token = keycloak?.token || "";
-
-          if (token != "" && token != null) {
-            setCookie("proxyToken", token, 365);
-          }
-        }
-      }
-    },
-    {
-      enabled: !!keycloak.token,
-      refetchInterval: 5000,
-      refetchIntervalInBackground: true,
-      refetchOnWindowFocus: false,
-    }
-  );
+  }
+  return null;
 };
