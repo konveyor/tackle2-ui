feb0e11f8b2db091c54c17868d2b62733eb9f9d1
Gilles Dubreuil <gilles@redhat.com>
2022-09-20 17:24:03 +0200

    Add Azure target with 3rd party filtering (#406)
    * Add Azure target
    
    * Adjust target cards for filtering
    
    * use PROFILE instead of PRODUCT envar
    
    (cherry picked from commit 81e8c36fe324e45286c1ae695ec62a56238ec7bc)
    Signed-off-by: Gilles Dubreuil <gilles@redhat.com>

--- pkg/client/src/app/pages/applications/analysis-wizard/set-targets.tsx
+++ pkg/client/src/app/pages/applications/analysis-wizard/set-targets.tsx
@@ -9,27 +9,15 @@ import {
   GalleryItem,
 } from "@patternfly/react-core";
 import { useTranslation } from "react-i18next";
-import migrationIcon from "@app/images/Icon-Red_Hat-Migration-A-Black-RGB.svg";
-import appOnServerIcon from "@app/images/Icon-Red_Hat-App_on_server-A-Black-RGB.svg";
-import cloudIcon from "@app/images/Icon-Red_Hat-Cloud-A-Black-RGB.svg";
-import serverIcon from "@app/images/Icon-Red_Hat-Server-A-Black-RGB.svg";
-import mugIcon from "@app/images/Icon-Red_Hat-Mug-A-Black-RGB.svg";
-import multiplyIcon from "@app/images/Icon-Red_Hat-Multiply-A-Black-RGB.svg";
-
-import { SelectCard } from "./components/select-card";
 import { useFormContext } from "react-hook-form";
 
-export interface TransformationTargets {
-  label: string;
-  description?: string;
-  options: Map<string, string>;
-  icon?: React.ComponentType<any>;
-  iconSrc?: string;
-}
+import { SelectCard } from "./components/select-card";
+import { ITransformationTargets, transformationTargets } from "./targets";
 
 export const SetTargets: React.FunctionComponent = () => {
   const { t } = useTranslation();
 
+<<<<<<< HEAD:pkg/client/src/app/pages/applications/analysis-wizard/set-targets.tsx
   const options: TransformationTargets[] = [
     {
       label: "Application server migration to",
@@ -105,16 +93,18 @@ export const SetTargets: React.FunctionComponent = () => {
     },
   ];
 
+=======
+>>>>>>> 81e8c36 (Add Azure target with 3rd party filtering (#406)):client/src/app/pages/applications/analysis-wizard/set-targets.tsx
   const { getValues, setValue } = useFormContext();
   const targets: string[] = getValues("targets");
 
   const handleOnCardChange = (
     isNewCard: boolean,
     selectionValue: string,
-    card: TransformationTargets
+    card: ITransformationTargets
   ) => {
     const selectedTargets = targets.filter(
-      (target) => !card.options.has(target)
+      (target) => !card.options.includes(target)
     );
 
     if (isNewCard) setValue("targets", [...selectedTargets, selectionValue]);
@@ -132,11 +122,11 @@ export const SetTargets: React.FunctionComponent = () => {
       <Stack>
         <StackItem>
           <Gallery hasGutter>
-            {options.map((elem, index) => (
+            {transformationTargets.map((elem, index) => (
               <GalleryItem key={index}>
                 <SelectCard
                   item={elem}
-                  cardSelected={[...elem.options.keys()].some((key) =>
+                  cardSelected={[...elem.options].some((key) =>
                     targets.includes(key)
                   )}
                   onChange={(isNewCard: boolean, selectionValue: string) => {
