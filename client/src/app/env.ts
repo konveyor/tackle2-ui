d9f35d256f0d0e1338a856dff62529025a94664c
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-09-23 14:08:01 -0400

    Simplify by inferring env var keys from defaults object
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/env.ts
+++ client/src/app/env.ts
@@ -1,22 +1,15 @@
-const ENV_VAR_KEYS = [
-  "AUTH_REQUIRED",
-  "KEYCLOAK_REALM",
-  "KEYCLOAK_CLIENT_ID",
-  "KEYCLOAK_SERVER_URL",
-  "UI_INGRESS_PROXY_BODY_SIZE",
-  "PROFILE",
-] as const;
-
-export type EnvVars = Record<typeof ENV_VAR_KEYS[number], string>;
-
-const ENV_DEFAULTS: EnvVars = {
+const ENV_VAR_DEFAULTS = {
   AUTH_REQUIRED: "true",
   KEYCLOAK_REALM: "tackle",
   KEYCLOAK_CLIENT_ID: "tackle-ui",
   KEYCLOAK_SERVER_URL: "",
   UI_INGRESS_PROXY_BODY_SIZE: "",
   PROFILE: "konveyor",
-};
+} as const;
+
+type EnvVarKey = keyof typeof ENV_VAR_DEFAULTS;
+const ENV_VAR_KEYS = Object.keys(ENV_VAR_DEFAULTS) as EnvVarKey[];
+export type EnvVars = Record<EnvVarKey, string>;
 
 declare global {
   interface Window {
@@ -26,5 +19,5 @@ declare global {
 export const ENV: EnvVars = window._env || {};
 
 ENV_VAR_KEYS.forEach((key) => {
-  if (!ENV[key]) ENV[key] = ENV_DEFAULTS[key];
+  if (!ENV[key]) ENV[key] = ENV_VAR_DEFAULTS[key];
 });
