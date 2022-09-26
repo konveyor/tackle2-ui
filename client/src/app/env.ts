32c501dda80bcd7316dee6da2ca5df7b77bbcf78
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-09-26 11:09:50 -0400

    Define defaults for missing ENV variables (#440)
    * Define defaults for missing ENV variables
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>
    
    * Add defaults for realm and client_id
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>
    
    * Simplify by inferring env var keys from defaults object
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>
    
    * Spread from window._env so we aren't mutating it directly and we can still inspect what was passed from the server
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

---
+++ client/src/app/env.ts
@@ -0,0 +1,23 @@
+const ENV_VAR_DEFAULTS = {
+  AUTH_REQUIRED: "true",
+  KEYCLOAK_REALM: "tackle",
+  KEYCLOAK_CLIENT_ID: "tackle-ui",
+  KEYCLOAK_SERVER_URL: "",
+  UI_INGRESS_PROXY_BODY_SIZE: "",
+  PROFILE: "konveyor",
+} as const;
+
+type EnvVarKey = keyof typeof ENV_VAR_DEFAULTS;
+const ENV_VAR_KEYS = Object.keys(ENV_VAR_DEFAULTS) as EnvVarKey[];
+export type EnvVars = Record<EnvVarKey, string>;
+
+declare global {
+  interface Window {
+    _env: EnvVars;
+  }
+}
+export const ENV: EnvVars = { ...window._env } || {};
+
+ENV_VAR_KEYS.forEach((key) => {
+  if (!ENV[key]) ENV[key] = ENV_VAR_DEFAULTS[key];
+});
