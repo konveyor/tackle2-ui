2e7d29976b1e16c4069ea17e94f44762bae2904c
ibolton336 <ibolton@redhat.com>
2022-10-21 14:23:33 -0400

    - Add react-error-boundary library - Clear error boundary on nav - Allow nav on error boundary - Show notification of app error in error boundary
    Signed-off-by: ibolton336 <ibolton@redhat.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,8 +7,6 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import App from "@app/App";
 import reportWebVitals from "@app/reportWebVitals";
 import configureStore from "@app/store";
-import i18n from "@app/i18n";
-import { NinjaErrorBoundary } from "@app/ninja-error-boundary";
 import { KeycloakProvider } from "@app/common/KeycloakProvider";
 
 const queryClient = new QueryClient();
@@ -16,12 +14,10 @@ const queryClient = new QueryClient();
 ReactDOM.render(
   <KeycloakProvider>
     <Provider store={configureStore()}>
-      <NinjaErrorBoundary>
-        <QueryClientProvider client={queryClient}>
-          <App />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </NinjaErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </KeycloakProvider>,
   document.getElementById("root")
