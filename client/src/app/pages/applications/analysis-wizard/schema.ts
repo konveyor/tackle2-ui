84a55a26293f05704fbf3ba151c59eebddb79f1e
Gilles Dubreuil <gilles@redhat.com>
2022-11-07 22:01:55 +0100

    UploadBinary: Fix artifact state
    Signed-off-by: Gilles Dubreuil <gilles@redhat.com>

--- client/src/app/pages/applications/analysis-wizard/schema.ts
+++ client/src/app/pages/applications/analysis-wizard/schema.ts
@@ -16,7 +16,7 @@ export type AnalysisScope = "app" | "app,oss" | "app,oss,select";
 
 export interface ModeStepValues {
   mode: AnalysisMode;
-  artifact: string;
+  artifact: File;
 }
 
 const useModeStepSchema = ({
@@ -41,13 +41,7 @@ const useModeStepSchema = ({
           return analyzableApplications.length > 0;
         }
       ),
-    artifact: yup
-      .string()
-      .defined()
-      .when("mode", {
-        is: "binary-upload",
-        then: (schema) => schema.required(),
-      }),
+    artifact: yup.object() as yup.SchemaOf<File>,
   });
 };
 
