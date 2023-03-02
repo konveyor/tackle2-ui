b9d8ac341fa7a37c361ae00d76f90e65bbc4cd5d
Gilles Dubreuil <gdubreui@redhat.com>
2023-03-02 17:09:51 +0100

    :bug: mvn: Refactor without useEffect (#678)
    * Refactor without useEffect
    * Use desctructing
    * Use isFetching and not isLoading

--- client/src/app/queries/cache.ts
+++ client/src/app/queries/cache.ts
@@ -1,37 +1,23 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 import { deleteCache, getCache } from "@app/api/rest";
-import { Cache } from "@app/api/models";
 
 export const CacheQueryKey = "cache";
-
 export const CleanProgressQueryKey = "cleanProgress";
 
-export const useFetchCache = () => {
-  const { data, isLoading, error, refetch } = useQuery<Cache>(
-    [CacheQueryKey],
-    async () => (await getCache()).data,
-    {
-      onError: (error) => console.log("error, ", error),
-    }
-  );
-  return {
-    cache: data || null,
-    isFetching: isLoading,
-    fetchError: error,
-    refetch,
-  };
-};
+export const useFetchCache = () =>
+  useQuery({
+    queryKey: [CacheQueryKey],
+    queryFn: getCache,
+    onError: (error) => console.log("error, ", error),
+  });
+
 export const useDeleteCacheMutation = (
   onSuccess: () => void,
   onError: (err: Error | null) => void
-) => {
-  return useMutation(deleteCache, {
-    onSuccess: () => {
-      onSuccess && onSuccess();
-    },
-    onError: (err: Error) => {
-      onError && onError(err);
-    },
+) =>
+  useMutation({
+    mutationFn: deleteCache,
+    onSuccess: onSuccess,
+    onError: onError,
   });
-};
