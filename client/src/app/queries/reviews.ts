70b331bd3949f94d43d3c23742502e0737843436
Gilles Dubreuil <gdubreui@redhat.com>
2023-03-20 17:00:25 +0100

    :bug: Fix deletion for Applications assessment/review  (#715) (#737)
    * No discard option unless there is an assessment or review
    * Fix assessment/review deletion
    * Refetch applications
    * Refresh data
    * force application fetch
    * force fetch after assessment discard too
    * Handling success or error callbacks
    * With application name for success
    * use interface instead of any
    * Reducing arguments
    * remove async
    * cleanup
    
    (cherry picked from commit 8b3574d5d1ff89ae699db664d89566aa91dc169c)

--- client/src/app/queries/reviews.ts
+++ client/src/app/queries/reviews.ts
@@ -1,7 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { getReviews } from "@app/api/rest";
+import { deleteReview, getReviews } from "@app/api/rest";
 import { Review } from "@app/api/models";
+import { AxiosError, AxiosResponse } from "axios";
 
 export interface IReviewFetchState {
   reviews: Review[];
@@ -26,4 +27,25 @@ export const useFetchReviews = (): IReviewFetchState => {
     fetchError: error,
     refetch,
   };
+};
+
+export interface IReviewMutation {
+  id: number;
+  name: string;
+}
+
+export const useDeleteReviewMutation = (
+  onSuccess: (name: string) => void,
+  onError: (err: AxiosError) => void
+) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (args: IReviewMutation) => deleteReview(args.id),
+    onSuccess: (_, args) => {
+      onSuccess(args.name);
+      queryClient.invalidateQueries([reviewsQueryKey]);
+    },
+    onError: onError,
+  });
 };
