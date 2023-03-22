33d2ba1b08b9c7c9f77b2193f8d7bbb42df12bb8
Ian Bolton <ibolton@redhat.com>
2023-03-22 09:02:04 -0400

    :bug: Remove wizard validation for rulebundle length
    Signed-off-by: Ian Bolton <ibolton@redhat.com>

--- client/src/app/pages/applications/analysis-wizard/analysis-wizard.tsx
+++ client/src/app/pages/applications/analysis-wizard/analysis-wizard.tsx
@@ -281,10 +281,6 @@ export const AnalysisWizard: React.FC<IAnalysisWizard> = ({
   const isModeValid = applications.every((app) => isModeSupported(app, mode));
 
   const onSubmit = (fieldValues: AnalysisWizardFormValues) => {
-    if (fieldValues.formRuleBundles.length < 1) {
-      console.log("Invalid form");
-      return;
-    }
     if (currentTaskgroup) {
       const taskgroup = setupTaskgroup(currentTaskgroup, fieldValues);
 
