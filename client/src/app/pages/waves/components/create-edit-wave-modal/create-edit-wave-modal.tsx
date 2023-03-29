6ce901868d6dec68325df5795c71dbed32a1a8e0
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-03-29 13:51:59 -0400

    :sparkles: Add skeleton for new Create Migration Wave modal with an edit mode (#740)
    * Add skeleton for new Create modal with an edit mode
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>
    
    * WIP: form boilerplate
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>
    
    * Simplify edit mode state to include the wave being edited
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>
    
    ---------
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

---
+++ client/src/app/pages/waves/components/create-edit-wave-modal/create-edit-wave-modal.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { AxiosResponse } from "axios";
+import { Modal, ModalVariant } from "@patternfly/react-core";
+
+import { WaveForm } from "./wave-form";
+
+type MigrationWave = any; // TODO add a real MigrationWave type and queries
+
+export interface CreateEditWaveModalProps {
+  isOpen: boolean;
+  waveBeingEdited: MigrationWave;
+  onSaved: (response: AxiosResponse<unknown>) => void;
+  onCancel: () => void;
+}
+
+export const CreateEditWaveModal: React.FC<CreateEditWaveModalProps> = ({
+  isOpen,
+  waveBeingEdited,
+  onSaved,
+  onCancel,
+}) => {
+  if (!isOpen) return null;
+  return (
+    <Modal
+      title={!waveBeingEdited ? "Create migration wave" : "Edit migration wave"}
+      variant={ModalVariant.small}
+      isOpen
+      onClose={onCancel}
+    >
+      <WaveForm onCancel={onCancel} />
+    </Modal>
+  );
+};
