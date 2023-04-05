90dae08ceb38e5a09385e9233706cb3cff7e95be
Ian Bolton <ibolton@redhat.com>
2023-04-05 17:14:19 -0400

    :seedling: Place jira integration behind waves feature flag (#802)
    Signed-off-by: ibolton336 <ibolton@redhat.com>

--- client/src/app/layout/SidebarApp/SidebarApp.tsx
+++ client/src/app/layout/SidebarApp/SidebarApp.tsx
@@ -163,18 +163,20 @@ export const SidebarApp: React.FC = () => {
               Custom migration targets
             </NavLink>
           </NavItem>
-          <NavExpandable
-            title="Issue management"
-            srText="SR Link"
-            groupId="admin-issue-management"
-            isExpanded
-          >
-            <NavItem>
-              <NavLink to={Paths.jira} activeClassName="pf-m-current">
-                Jira
-              </NavLink>
-            </NavItem>
-          </NavExpandable>
+          {FEATURES_ENABLED.migrationWaves ? (
+            <NavExpandable
+              title="Issue management"
+              srText="SR Link"
+              groupId="admin-issue-management"
+              isExpanded
+            >
+              <NavItem>
+                <NavLink to={Paths.jira} activeClassName="pf-m-current">
+                  Jira
+                </NavLink>
+              </NavItem>
+            </NavExpandable>
+          ) : null}
         </NavList>
       )}
     </Nav>
