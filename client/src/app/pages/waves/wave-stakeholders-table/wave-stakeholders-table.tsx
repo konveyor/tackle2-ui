604683abbb74b08e9a722c319418de3bbf14a290
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-04-11 17:20:15 -0400

    Converting jira trackers table
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/pages/waves/wave-stakeholders-table/wave-stakeholders-table.tsx
+++ client/src/app/pages/waves/wave-stakeholders-table/wave-stakeholders-table.tsx
@@ -79,6 +79,7 @@ export const WaveStakeholdersTable: React.FC<IWaveStakeholdersTableProps> = ({
       >
         <Thead>
           {/* TODO is there any way we can abstract this Thead into a component? how do we make sure we can still put modifier props on the Th for each column? */}
+          {/* TODO implement sorting first so we can see how that fits into the abstraction */}
           {Object.keys(columnNames).map((columnKey) => (
             <Th key={columnKey}>
               {columnNames[columnKey as keyof typeof columnNames]}
--- client/src/app/pages/waves/waves.tsx
+++ client/src/app/pages/waves/waves.tsx
@@ -197,6 +197,7 @@ export const Waves: React.FC = () => {
             <TableComposable {...tableProps} aria-label="Migration waves table">
               <Thead>
                 {/* TODO is there any way we can abstract this Thead into a component? how do we make sure we can still put modifier props on the Th for each column? */}
+                {/* TODO implement sorting first so we can see how that fits into the abstraction */}
                 <Th /* Checkbox column */ />
                 {Object.keys(columnNames).map((columnKey) => (
                   <Th key={columnKey}>
