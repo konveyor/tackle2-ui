40831328985cc1daf5acde2726d937f531a1a2fe
ibolton336 <ibolton@redhat.com>
2023-05-24 20:14:38 -0400

    :ghost: Add support for xml & yaml rule files in tandem
    Signed-off-by: ibolton336 <ibolton@redhat.com>

--- client/src/app/common/CustomRules/rules-utils.tsx
+++ client/src/app/common/CustomRules/rules-utils.tsx
@@ -1,25 +1,82 @@
 import { IReadFile, ParsedRule, Rule, Ruleset } from "@app/api/models";
 import yaml from "js-yaml";
+type RuleFileType = "YAML" | "XML" | null;
+export const checkRuleFileType = (filename: string): RuleFileType => {
+  const fileExtension = filename.split(".").pop();
+  if (fileExtension === ("yaml" || "yml")) {
+    return "YAML";
+  } else if (fileExtension === "xml") {
+    return "XML";
+  } else return null;
+};
 
 export const parseRules = (file: IReadFile): ParsedRule => {
   if (file.data) {
-    const payload = atob(file.data.substring(31));
-    const yamlDoc = yaml.load(payload) as any[];
-    const yamlLabels = yamlDoc?.reduce((acc, parsedLine) => {
-      const newLabels = parsedLine?.labels ? parsedLine?.labels : [];
-      return [...acc, ...newLabels];
-    }, []);
-    const allLabels = getLabels(yamlLabels);
-    return {
-      source: allLabels?.sourceLabel,
-      target: allLabels?.targetLabel,
-      otherLabels: allLabels?.otherLabels,
-      allLabels: allLabels?.allLabels,
-      total: 0,
-      ...(file.responseID && {
-        fileID: file.responseID,
-      }),
-    };
+    if (checkRuleFileType(file.fileName) === "YAML") {
+      const payload = atob(file.data.substring(31));
+      const yamlDoc = yaml.load(payload) as any[];
+      const yamlLabels = yamlDoc?.reduce((acc, parsedLine) => {
+        const newLabels = parsedLine?.labels ? parsedLine?.labels : [];
+        return [...acc, ...newLabels];
+      }, []);
+      const allLabels = getLabels(yamlLabels);
+      return {
+        source: allLabels?.sourceLabel,
+        target: allLabels?.targetLabel,
+        otherLabels: allLabels?.otherLabels,
+        allLabels: allLabels?.allLabels,
+        total: 0,
+        ...(file.responseID && {
+          fileID: file.responseID,
+        }),
+      };
+    } else if (checkRuleFileType(file.fileName) === "XML") {
+      let source: string | null = null;
+      let target: string | null = null;
+      let rulesCount = 0;
+
+      const payload = atob(file.data.substring(21));
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(payload, "text/xml");
+
+      const ruleSets = xml.getElementsByTagName("ruleset");
+
+      if (ruleSets && ruleSets.length > 0) {
+        const metadata = ruleSets[0].getElementsByTagName("metadata");
+
+        if (metadata && metadata.length > 0) {
+          const sources = metadata[0].getElementsByTagName("sourceTechnology");
+          if (sources && sources.length > 0) source = sources[0].id;
+
+          const targets = metadata[0].getElementsByTagName("targetTechnology");
+          if (targets && targets.length > 0) target = targets[0].id;
+        }
+
+        const rulesGroup = ruleSets[0].getElementsByTagName("rules");
+        if (rulesGroup && rulesGroup.length > 0)
+          rulesCount = rulesGroup[0].getElementsByTagName("rule").length;
+      }
+      const allLabels = [
+        ...(source ? [`konveyor.io/source=${source}`] : []),
+        ...(target ? [`konveyor.io/target=${target}`] : []),
+      ];
+      return {
+        source: source,
+        target: target,
+        otherLabels: allLabels,
+        allLabels: allLabels,
+        total: 0,
+        ...(file.responseID && {
+          fileID: file.responseID,
+        }),
+      };
+    } else {
+      return {
+        source: null,
+        target: null,
+        total: 0,
+      };
+    }
   } else {
     return {
       source: null,
