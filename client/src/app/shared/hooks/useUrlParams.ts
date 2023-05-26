dc26cb684cde9d82dcececc83348d60270d946a8
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-05-26 17:33:35 -0400

    Add keyPrefix support to useUrlParams
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/shared/hooks/useUrlParams.ts
+++ client/src/app/shared/hooks/useUrlParams.ts
@@ -2,17 +2,36 @@ import { objectKeys } from "@app/utils/utils";
 import React from "react";
 import { useLocation, useHistory } from "react-router-dom";
 
+// useUrlParams is a generic hook similar to React.useState which stores its state in the URL search params string.
+// The state is retained on a page reload, when using the browser back/forward buttons, or when bookmarking the page.
+// It can be used to store a value of any type (`TDeserializedParams`) in one or more URL params by providing:
+// - A list of `keys` you want to use for the URL params
+// - A `serialize` function to convert this type into an object with string values (`TSerializedParams`)
+// - A `deserialize` function to convert the serialized object back to your state's type
+// - An optional `keyPrefix` to allow for multiple instances using the same keys on the same page.
+// The return value is the same [value, setValue] tuple returned by React.useState.
+
+// Note: You do not need to worry about the keyPrefix in your serialize and deserialize functions.
+//       The keys of TDeserializedParams and TSerializedParams have the prefixes omitted.
+//       Prefixes are only used at the very first/last step when reading/writing from/to the URLSearchParams object.
+
+type TSerializedParams<TUrlParamKey extends string> = Partial<
+  Record<TUrlParamKey, string | null>
+>;
+
 export interface IUseUrlParamsArgs<
   TUrlParamKey extends string,
+  TKeyPrefix extends string,
   TDeserializedParams
 > {
+  keyPrefix?: TKeyPrefix;
   keys: TUrlParamKey[];
   defaultValue: TDeserializedParams;
   serialize: (
     params: Partial<TDeserializedParams>
-  ) => Partial<Record<TUrlParamKey, string | null>>;
+  ) => TSerializedParams<TUrlParamKey>;
   deserialize: (
-    urlParams: Partial<Record<TUrlParamKey, string>>
+    serializedParams: TSerializedParams<TUrlParamKey>
   ) => TDeserializedParams;
 }
 
@@ -21,40 +40,67 @@ export type TUrlParamStateTuple<TDeserializedParams> = readonly [
   (newParams: Partial<TDeserializedParams>) => void
 ];
 
-export const useUrlParams = <TUrlParamKey extends string, TDeserializedParams>({
+export const useUrlParams = <
+  TUrlParamKey extends string,
+  TKeyPrefix extends string,
+  TDeserializedParams
+>({
+  keyPrefix,
   keys,
   defaultValue,
   serialize,
   deserialize,
 }: IUseUrlParamsArgs<
   TUrlParamKey,
+  TKeyPrefix,
   TDeserializedParams
 >): TUrlParamStateTuple<TDeserializedParams> => {
+  type TPrefixedUrlParamKey = TUrlParamKey | `${TKeyPrefix}:${TUrlParamKey}`;
+
   const history = useHistory();
+
+  const withPrefix = (key: TUrlParamKey): TPrefixedUrlParamKey =>
+    keyPrefix ? `${keyPrefix}:${key}` : key;
+
+  const withPrefixes = (
+    serializedParams: TSerializedParams<TUrlParamKey>
+  ): TSerializedParams<TPrefixedUrlParamKey> =>
+    keyPrefix
+      ? objectKeys(serializedParams).reduce(
+          (obj, key) => ({
+            ...obj,
+            [withPrefix(key)]: serializedParams[key],
+          }),
+          {} as TSerializedParams<TPrefixedUrlParamKey>
+        )
+      : (serializedParams as TSerializedParams<TPrefixedUrlParamKey>);
 
   const setParams = (newParams: Partial<TDeserializedParams>) => {
     // In case setParams is called multiple times synchronously from the same rendered instance,
     // we use document.location here as the current params so these calls never overwrite each other.
     // This also retains any unrelated params that might be present and allows newParams to be a partial update.
     const { pathname, search } = document.location;
-    const existingSerializedParams = Object.fromEntries(
-      new URLSearchParams(search)
-    );
-    const newSerializedParams = serialize(newParams);
+    const existingSearchParams = new URLSearchParams(search);
+    // We prefix the params object here so the serialize function doesn't have to care about the keyPrefix.
+    const newPrefixedSerializedParams = withPrefixes(serialize(newParams));
     history.replace({
       pathname,
       search: trimAndStringifyUrlParams({
-        existingParams: existingSerializedParams,
-        params: newSerializedParams,
+        existingSearchParams,
+        newPrefixedSerializedParams,
       }),
     });
   };
 
-  // We use useLocation here so we are re-rendering when the params change
+  // We use useLocation here so we are re-rendering when the params change.
   const urlParams = new URLSearchParams(useLocation().search);
+  // We un-prefix the params object here so the deserialize function doesn't have to care about the keyPrefix.
   const serializedParams = keys.reduce(
-    (obj, key) => ({ ...obj, [key]: urlParams.get(key) }),
-    {} as Partial<Record<TUrlParamKey, string>>
+    (obj, key) => ({
+      ...obj,
+      [key]: urlParams.get(withPrefix(key)),
+    }),
+    {} as TSerializedParams<TUrlParamKey>
   );
   const allParamsEmpty = keys.every((key) => !serializedParams[key]);
   const params = allParamsEmpty ? defaultValue : deserialize(serializedParams);
@@ -66,25 +112,30 @@ export const useUrlParams = <TUrlParamKey extends string, TDeserializedParams>({
   return [params, setParams];
 };
 
-export const trimAndStringifyUrlParams = <TUrlParamKey extends string>({
-  existingParams = {},
-  params,
+export const trimAndStringifyUrlParams = <TPrefixedUrlParamKey extends string>({
+  existingSearchParams = new URLSearchParams(),
+  newPrefixedSerializedParams,
 }: {
-  existingParams?: Record<string, string>;
-  params: Partial<Record<TUrlParamKey, string | null>>;
+  existingSearchParams?: URLSearchParams;
+  newPrefixedSerializedParams: TSerializedParams<TPrefixedUrlParamKey>;
 }) => {
-  objectKeys(params).forEach((key) => {
+  const existingPrefixedSerializedParams =
+    Object.fromEntries(existingSearchParams);
+  objectKeys(newPrefixedSerializedParams).forEach((key) => {
     // Returning undefined for a property from serialize should result in it being omitted from the partial update.
-    if (params[key] === undefined) {
-      delete params[key];
+    if (newPrefixedSerializedParams[key] === undefined) {
+      delete newPrefixedSerializedParams[key];
     }
     // Returning null for a property from serialize should result in it being removed from the URL.
-    if (params[key] === null) {
-      delete params[key];
-      delete existingParams[key];
+    if (newPrefixedSerializedParams[key] === null) {
+      delete newPrefixedSerializedParams[key];
+      delete existingPrefixedSerializedParams[key];
     }
   });
-  const newParams = new URLSearchParams({ ...existingParams, ...params });
+  const newParams = new URLSearchParams({
+    ...existingPrefixedSerializedParams,
+    ...newPrefixedSerializedParams,
+  });
   newParams.sort();
   return newParams.toString();
 };
