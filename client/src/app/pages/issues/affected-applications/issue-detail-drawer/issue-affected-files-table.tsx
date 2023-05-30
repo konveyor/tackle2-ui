d8dcfbc5decb389aa40dd04b5267d95399cb6282
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-05-30 14:43:32 -0400

    Initial structure for affected files table
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/pages/issues/affected-applications/issue-detail-drawer/issue-affected-files-table.tsx
+++ client/src/app/pages/issues/affected-applications/issue-detail-drawer/issue-affected-files-table.tsx
@@ -1,7 +1,28 @@
 import * as React from "react";
+import { useSelectionState } from "@migtools/lib-ui";
+import {
+  TableComposable,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@patternfly/react-table";
 import { AnalysisIssueReport } from "@app/api/models";
-import { getHubRequestParams } from "@app/shared/hooks/table-controls";
+import {
+  getHubRequestParams,
+  useTableControlProps,
+  useTableControlUrlParams,
+} from "@app/shared/hooks/table-controls";
 import { useFetchFileReports } from "@app/queries/issues";
+import { TableURLParamKeyPrefix } from "@app/Constants";
+import {
+  ConditionalTableBody,
+  TableHeaderContentWithControls,
+  TableRowContentWithControls,
+} from "@app/shared/components/table-controls";
+import { SimplePagination } from "@app/shared/components/simple-pagination";
+import { Toolbar, ToolbarContent, ToolbarItem } from "@patternfly/react-core";
 
 export interface IIssueAffectedFilesTableProps {
   issueReport: AnalysisIssueReport;
@@ -10,17 +31,126 @@ export interface IIssueAffectedFilesTableProps {
 export const IssueAffectedFilesTable: React.FC<
   IIssueAffectedFilesTableProps
 > = ({ issueReport }) => {
-  // TODO useTableControlUrlParams
-  // TODO do we need to put these in the URL namespaced/prefixed so they can coexist with the main page table? (yes)
+  const tableControlState = useTableControlUrlParams({
+    urlParamKeyPrefix: TableURLParamKeyPrefix.affectedFiles,
+    columnNames: {
+      file: "File",
+      incidents: "Incidents",
+      effort: "Effort",
+    },
+    sortableColumns: [
+      "file",
+      "incidents",
+      // "effort" // TODO this doesn't currently work in the hub
+    ],
+    initialSort: {
+      columnKey: "file",
+      direction: "asc",
+    },
+    initialItemsPerPage: 10,
+    variant: "compact",
+  });
+
   const {
-    result: { data: currentPageFileReports, total: totalItemCount },
+    result: { data: currentPageItems, total: totalItemCount },
     isFetching,
     fetchError,
-  } = useFetchFileReports(issueReport.id, getHubRequestParams({})); // TODO pass params from above
+  } = useFetchFileReports(
+    issueReport.id,
+    getHubRequestParams({
+      ...tableControlState,
+      hubSortFieldKeys: {
+        file: "file",
+        incidents: "incidents",
+        // effort: "effort", // TODO this doesn't currently work in the hub
+      },
+    })
+  );
 
-  // TODO useTableControlProps
+  const tableControls = useTableControlProps({
+    ...tableControlState,
+    idProperty: "issueId",
+    currentPageItems,
+    totalItemCount,
+    isLoading: isFetching,
+    // TODO FIXME - we don't need selectionState but it's required by this hook?
+    selectionState: useSelectionState({
+      items: currentPageItems,
+      isEqual: (a, b) => a.issueId === b.issueId,
+    }),
+  });
+
+  const {
+    numRenderedColumns,
+    propHelpers: {
+      toolbarProps,
+      paginationToolbarItemProps,
+      paginationProps,
+      tableProps,
+      getThProps,
+      getTdProps,
+    },
+  } = tableControls;
 
   // TODO search toolbar at the top
 
-  return <>TODO</>;
+  return (
+    <>
+      <Toolbar {...toolbarProps}>
+        <ToolbarContent>
+          <ToolbarItem {...paginationToolbarItemProps}>
+            <SimplePagination
+              idPrefix="affected-files-table"
+              isTop
+              paginationProps={paginationProps}
+            />
+          </ToolbarItem>
+        </ToolbarContent>
+      </Toolbar>
+      <TableComposable {...tableProps} aria-label="Affected files table">
+        <Thead>
+          <Tr>
+            <TableHeaderContentWithControls {...tableControls}>
+              <Th {...getThProps({ columnKey: "file" })} />
+              <Th {...getThProps({ columnKey: "incidents" })} />
+              <Th {...getThProps({ columnKey: "effort" })} />
+            </TableHeaderContentWithControls>
+          </Tr>
+        </Thead>
+        <ConditionalTableBody
+          isLoading={isFetching}
+          isError={!!fetchError}
+          isNoData={totalItemCount === 0}
+          numRenderedColumns={numRenderedColumns}
+        >
+          <Tbody>
+            {currentPageItems?.map((issueReport, rowIndex) => (
+              <Tr key={issueReport.issueId}>
+                <TableRowContentWithControls
+                  {...tableControls}
+                  item={issueReport}
+                  rowIndex={rowIndex}
+                >
+                  <Td width={70} {...getTdProps({ columnKey: "file" })}>
+                    TODO: link: {issueReport.file}
+                  </Td>
+                  <Td width={10} {...getTdProps({ columnKey: "incidents" })}>
+                    {issueReport.incidents}
+                  </Td>
+                  <Td width={20} {...getTdProps({ columnKey: "effort" })}>
+                    TODO: effort
+                  </Td>
+                </TableRowContentWithControls>
+              </Tr>
+            ))}
+          </Tbody>
+        </ConditionalTableBody>
+      </TableComposable>
+      <SimplePagination
+        idPrefix="affected-files-table"
+        isTop={false}
+        paginationProps={paginationProps}
+      />
+    </>
+  );
 };
