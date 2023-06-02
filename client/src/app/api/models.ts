0c052a8b1ae23763486790cee10226a5356a0376
Ian Bolton <ibolton@redhat.com>
2023-06-02 10:52:13 -0400

    :ghost: Support both xml and yaml files for custom rules (#930)
    - Adds support for XML rule files alongside yaml rule files.
    Conditionally reinstates the xml validation based on rule type
    - Adds label parser function for source/target label display purposes in
    the wizard
    
    ---------
    
    Signed-off-by: ibolton336 <ibolton@redhat.com>

--- client/src/app/api/models.ts
+++ client/src/app/api/models.ts
@@ -400,7 +400,10 @@ export interface TaskData {
     rulesets: Ref[];
     repository?: Repository;
     identity?: Ref;
-    labels: string[];
+    labels: {
+      included: string[];
+      excluded: string[];
+    };
   };
 }
 
