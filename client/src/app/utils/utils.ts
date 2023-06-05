fbe9726cefd67ea416845834cfe83bdb7ee4800b
Ian Bolton <ibolton@redhat.com>
2023-06-05 05:32:21 -0400

    :bug: Require https for jira instances (#976)
    Closes #969
    
    (cherry picked from commit 85ca0a4b6dbd1daa29ac77dce62c1d46d57c0679)
    Signed-off-by: Gilles Dubreuil <gdubreui@redhat.com>

--- client/src/app/utils/utils.ts
+++ client/src/app/utils/utils.ts
@@ -96,3 +96,6 @@ export const getValidatedFromError = (
 
 export const standardURLRegex =
   /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+
+export const standardStrictURLRegex =
+  /https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
