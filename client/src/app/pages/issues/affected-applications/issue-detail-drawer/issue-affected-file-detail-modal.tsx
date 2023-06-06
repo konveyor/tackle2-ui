dd0ba2d31ea9ef2fa2579ee6a27d1af660595b0a
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-06-06 15:17:24 -0400

    Set up CodeEditor and render codeSnip
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/pages/issues/affected-applications/issue-detail-drawer/issue-affected-file-detail-modal.tsx
+++ client/src/app/pages/issues/affected-applications/issue-detail-drawer/issue-affected-file-detail-modal.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
-import { Button, Modal, Tab, Tabs } from "@patternfly/react-core";
+import { useTranslation } from "react-i18next";
+import {
+  Button,
+  Grid,
+  GridItem,
+  Modal,
+  Tab,
+  Tabs,
+} from "@patternfly/react-core";
+import spacing from "@patternfly/react-styles/css/utilities/Spacing/spacing";
+import { CodeEditor, Language } from "@patternfly/react-code-editor";
 import { AnalysisFileReport } from "@app/api/models";
 import { useFetchIssueIncidents } from "@app/queries/issues";
 import {
   AppPlaceholder,
-  ConditionalRender,
   NoDataEmptyState,
   StateError,
 } from "@app/shared/components";
-import { useTranslation } from "react-i18next";
 
 export interface IIssueAffectedFileDetailModalProps {
   fileReport: AnalysisFileReport;
@@ -73,7 +81,7 @@ export const IssueAffectedFileDetailModal: React.FC<
         <Tabs
           activeKey={activeTabIncidentId}
           onSelect={(_event, tabKey) =>
-            setActiveTabIncidentId(tabKey as number | "all")
+            setActiveTabIncidentId(tabKey as TabKey)
           }
         >
           {firstFiveIncidents.map((incident, index) => (
@@ -82,11 +90,37 @@ export const IssueAffectedFileDetailModal: React.FC<
               eventKey={incident.id}
               title={`Incident #${index + 1}: Line ${incident.line}`} // TODO i18n
             >
-              TODO
+              <Grid hasGutter className={spacing.mtLg}>
+                <GridItem span={6}>
+                  <CodeEditor
+                    isDarkTheme
+                    isLineNumbersVisible
+                    isReadOnly
+                    code={incident.codeSnip}
+                    options={
+                      {
+                        // TODO figure out magic numbers here and make this accurate - use hub codeSnipStartLine once it exists?
+                        // lineNumbers: (lineNumber) =>
+                        //  String(incident.line + lineNumber - 1 - 10), // -1 because lineNumber is 1-indexed, - 10 because codeSnip starts 10 lines early
+                      }
+                    }
+                    height="450px"
+                    onEditorDidMount={(editor) => {
+                      editor.layout();
+                    }}
+                    // language={Language.java} // TODO can we determine the language from the hub?
+                    // TODO see monaco-editor-webpack-plugin setup info for including only resources for supported languages in the build
+                  />
+                </GridItem>
+                <GridItem span={6}>TODO</GridItem>
+              </Grid>
             </Tab>
           ))}
           {totalNumIncidents > 5 ? (
-            <Tab eventKey="all" title="All incidents">
+            <Tab
+              eventKey="all"
+              title={`All incidents (${totalNumIncidents})`} // TODO i18n
+            >
               TODO: All
             </Tab>
           ) : null}
