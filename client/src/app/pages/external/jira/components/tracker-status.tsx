a65998c4bc0ec45bbc0d4535f16e372ea1a8ce4f
Gilles Dubreuil <gdubreui@redhat.com>
2023-06-07 16:42:25 +0200

    :sparkles:  Jira configuration: instance error details (#982)
    Resolves https://issues.redhat.com/browse/MTA-697
    
    Signed-off-by: ibolton336 <ibolton@redhat.com>

--- client/src/app/pages/external/jira/components/tracker-status.tsx
+++ client/src/app/pages/external/jira/components/tracker-status.tsx
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusIcon } from "@migtools/lib-ui";
 
 import spacing from "@patternfly/react-styles/css/utilities/Spacing/spacing";
+import { useTranslation } from "react-i18next";
+import {
+  Button,
+  CodeBlock,
+  CodeBlockCode,
+  Modal,
+} from "@patternfly/react-core";
 
 interface ITrackerStatusProps {
   connected: boolean;
+  message: string;
 }
-const TrackerStatus = ({ connected }: ITrackerStatusProps) => {
+const TrackerStatus = ({ connected, message }: ITrackerStatusProps) => {
+  const { t } = useTranslation();
+  const [codeModalState, setCodeModalState] = useState<string | null>(null);
   return (
     <>
-      <StatusIcon status={connected ? "Ok" : "Error"} />
-      <span className={spacing.mlSm}>
-        {connected ? "Connected" : "Not connected "}
-      </span>
+      <StatusIcon
+        status={connected ? "Ok" : "Error"}
+        className={spacing.mlSm}
+        label={
+          connected ? (
+            "Connected"
+          ) : (
+            <Button
+              isInline
+              variant="link"
+              onClick={() => setCodeModalState(message)}
+            >
+              Not connected
+            </Button>
+          )
+        }
+      />
+      <Modal
+        title={t("composed.error", {
+          what: t("terms.instance"),
+        })}
+        width="50%"
+        isOpen={!!codeModalState}
+        onClose={() => setCodeModalState(null)}
+      >
+        <CodeBlock>
+          <CodeBlockCode id="code-content">{codeModalState}</CodeBlockCode>
+        </CodeBlock>
+      </Modal>
     </>
   );
 };
