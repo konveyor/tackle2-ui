bc09aa0e5351a8c07b91ca9a3e99993a2f57b49c
Gilles Dubreuil <gdubreui@redhat.com>
2023-07-07 03:21:55 +0200

    fix RiskLabel test for non updated styles.modifiers.<color>
    Signed-off-by: Gilles Dubreuil <gdubreui@redhat.com>

--- client/src/app/shared/components/risk-label/tests/risk-label.test.tsx
+++ client/src/app/shared/components/risk-label/tests/risk-label.test.tsx
@@ -5,17 +5,17 @@ import { RiskLabel } from "../risk-label";
 describe("RiskLabel", () => {
   it("Green", () => {
     const { container } = render(<RiskLabel risk="GREEN" />);
-    expect(container.firstChild).toHaveClass("pf-v5-c-label pf-v5-m-green");
+    expect(container.firstChild).toHaveClass("pf-v5-c-label pf-m-green");
     expect(screen.getByText("risks.low")).toBeInTheDocument();
   });
   it("Amber", () => {
     const { container } = render(<RiskLabel risk="AMBER" />);
-    expect(container.firstChild).toHaveClass("pf-v5-c-label pf-v5-m-orange");
+    expect(container.firstChild).toHaveClass("pf-v5-c-label pf-m-orange");
     expect(screen.getByText("risks.medium")).toBeInTheDocument();
   });
   it("Red", () => {
     const { container } = render(<RiskLabel risk="RED" />);
-    expect(container.firstChild).toHaveClass("pf-v5-c-label pf-v5-m-red");
+    expect(container.firstChild).toHaveClass("pf-v5-c-label pf-m-red");
     expect(screen.getByText("risks.high")).toBeInTheDocument();
   });
   it("Unknown", () => {
