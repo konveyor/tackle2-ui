9468fa705a91da49bdccc8b3b1e09b532e25745f
Scott Dickerson <sdickers@redhat.com>
2023-07-10 17:23:17 -0400

    :seedling: Enable webpack to do code splitting (#1075)
    ## Summary
    By updating `tsconfig.json` and making a few changes in webpack
    configuration, code splitting is now working.
    
    For developers, this means that the previous 100MB+ app bundle that
    could frequently crash my browser when trying to debug code has been
    replaced by a small initial bundle with dynamic loading of each
    React.lazy() component. Code debugging and source viewing works much
    better now.
    
    For end users, this means the app can start up faster. Less network
    traffic is great, but really only has an effect if the network latency
    is high (e.g. if the app is located on a different continent from the
    user).
    
    Resolves: #1074
    
    ## Change Details
    
    ### Refactor the client `tsconfig.json`
      - By updating the `tsconfig.json` to use `module: es2020`, this allows
        webpack to do actual code splitting based on `React.lazy()`.
    
      - Many of the previous settings that were removed are now redundant
        with either the `strict: true` or with the current `target`, `module`,
        and `moduleResolution` settings.
    
      - The `ts-node` section has been added to allow the webpack
        configuration files to be written in typescript.
    
    ### webpack config refactoring
      - Use `pathTo()` instead of `path.resolve(__dirname)`.  This
        will help any migration to use ESM modules in future.
    
      - Setup `output` file names for dev and prod builds
    
      - Have monaco workers generate to `monaco/...`
    
      - Use `CaseSensitivePathsPlugin` since it is a devDep but
        currently unused
    
      - Use consistent ordering for imports
    
    ### updates to `client/package.json`
      - Add script `build:dev`
    
      - Remove `GENERATE_SOURCEMAP=true` from the `start:dev` script since
        it does not impact the build at all
    
      - Rearrange the `prebuild` script, and force `--noEmit`
    
      - Drop dependencies that are not being used:
        - `webpack-manifest-plugin`
        - `react-dev-utils`
        - `react-refresh`
    
    ---------
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/config/webpack.prod.ts
+++ client/config/webpack.prod.ts
@@ -1,26 +1,37 @@
 import path from "path";
 import merge from "webpack-merge";
 import webpack, { Configuration } from "webpack";
-import commonWebpackConfiguration from "./webpack.common";
-import { stylePaths } from "./stylePaths";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
-import TerserJSPlugin from "terser-webpack-plugin";
+
+import { stylePaths } from "./stylePaths";
+import commonWebpackConfiguration from "./webpack.common";
 
 const brandType = process.env["PROFILE"] || "konveyor";
+const pathTo = (relativePath: string) => path.resolve(__dirname, relativePath);
 
 const config = merge<Configuration>(commonWebpackConfiguration, {
   mode: "production",
-  devtool: "nosources-source-map",
-  optimization: {
-    minimizer: [new TerserJSPlugin({}), `...`, new CssMinimizerPlugin()],
-    sideEffects: true,
+  devtool: "nosources-source-map", // used to map stack traces on the client without exposing all of the source code
+  output: {
+    filename: "[name].[contenthash:8].min.js",
+    chunkFilename: "js/[name].[chunkhash:8].min.js",
+    assetModuleFilename: "assets/[name].[contenthash:8][ext]",
   },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      "...", // The '...' string represents the webpack default TerserPlugin instance
+      new CssMinimizerPlugin(),
+    ],
+  },
+
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[name].bundle.css",
+      filename: "[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[chunkhash:8].min.css",
     }),
     new CssMinimizerPlugin({
       minimizerOptions: {
@@ -30,16 +41,14 @@ const config = merge<Configuration>(commonWebpackConfiguration, {
     new HtmlWebpackPlugin({
       // In real prod mode, populate window._env at run time with express
       filename: "index.html.ejs",
-      template: `!!raw-loader!${path.resolve(
-        __dirname,
-        "../public/index.html.ejs"
-      )}`,
-      favicon: path.resolve(__dirname, `../public/${brandType}-favicon.ico`),
+      template: `!!raw-loader!${pathTo("../public/index.html.ejs")}`,
+      favicon: pathTo(`../public/${brandType}-favicon.ico`),
     }),
     new webpack.EnvironmentPlugin({
       NODE_ENV: "production",
     }),
   ],
+
   module: {
     rules: [
       {
