7d6e39fde7438b78e2dc93e26ba710ca7e1e79b4
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-07-17 13:25:02 -0400

    Revert ":bug: Fix mismatched new/deprecated dropdown components in KebabDropdown (#1126)"
    This reverts commit e35086b8c089299acc1afdceda632bf2f5e6a647.
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/shared/components/kebab-dropdown/kebab-dropdown.tsx
+++ client/src/app/shared/components/kebab-dropdown/kebab-dropdown.tsx
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
-import {
-  Dropdown,
-  DropdownList,
-  MenuToggle,
-  MenuToggleElement,
-} from "@patternfly/react-core";
-import EllipsisVIcon from "@patternfly/react-icons/dist/esm/icons/ellipsis-v-icon";
+
+import { Dropdown, KebabToggle } from "@patternfly/react-core/deprecated";
 
 export interface IKebabDropdownProps {
-  dropdownItems?: React.ReactNode[];
+  dropdownItems?: any[];
 }
 
 export const KebabDropdown: React.FC<IKebabDropdownProps> = ({
@@ -16,25 +11,17 @@ export const KebabDropdown: React.FC<IKebabDropdownProps> = ({
 }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const onKebabToggle = (isOpen: boolean) => {
+    setIsOpen(isOpen);
+  };
+
   return (
     <Dropdown
-      popperProps={{ position: "right" }}
+      toggle={<KebabToggle onToggle={(_, isOpen) => onKebabToggle(isOpen)} />}
       isOpen={isOpen}
-      onOpenChange={(isOpen) => setIsOpen(isOpen)}
-      toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
-        <MenuToggle
-          ref={toggleRef}
-          isExpanded={isOpen}
-          onClick={() => setIsOpen(!isOpen)}
-          variant="plain"
-          aria-label="Table toolbar actions kebab toggle"
-          isDisabled={!dropdownItems || dropdownItems.length === 0}
-        >
-          <EllipsisVIcon aria-hidden="true" />
-        </MenuToggle>
-      )}
-    >
-      <DropdownList>{dropdownItems}</DropdownList>
-    </Dropdown>
+      isPlain
+      position="right"
+      dropdownItems={dropdownItems}
+    />
   );
 };
