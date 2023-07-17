1bd000f510d41e77fd4bc4b5d33bde886995800d
Gilles Dubreuil <gdubreui@redhat.com>
2023-07-17 21:34:41 +0200

    :bug: [backport release-0.2] Applications: bulk delete disabled when no selection (#1149) (#1153)
    Resolves https://issues.redhat.com/browse/MTA-898
    
    (cherry picked from commit 807e094caa87bb448479b0326dcd721c536468e5)

--- client/src/app/pages/applications/applications-table-assessment/applications-table-assessment.tsx
+++ client/src/app/pages/applications/applications-table-assessment/applications-table-assessment.tsx
@@ -606,6 +606,10 @@ export const ApplicationsTable: React.FC = () => {
     importWriteAccess = checkAccess(userScopes, importsWriteScopes),
     applicationWriteAccess = checkAccess(userScopes, applicationsWriteScopes);
 
+  const areAppsInWaves = selectedRows.some(
+    (application) => application.migrationWave !== null
+  );
+
   const importDropdownItems = importWriteAccess
     ? [
         <DropdownItem
@@ -629,18 +633,14 @@ export const ApplicationsTable: React.FC = () => {
     ? [
         <ConditionalTooltip
           key="delete-app-tooltip"
-          isTooltipEnabled={selectedRows.some(
-            (application) => application.migrationWave !== null
-          )}
+          isTooltipEnabled={areAppsInWaves}
           content={
             "Cannot delete application(s) assigned to migration wave(s)."
           }
         >
           <DropdownItem
             key="applications-bulk-delete"
-            isDisabled={selectedRows.some(
-              (application) => application.migrationWave !== null
-            )}
+            isAriaDisabled={areAppsInWaves || selectedRows.length < 1}
             onClick={() => {
               openBulkDeleteModal(selectedRows);
             }}
