c0154931113a2cdfb595917abeb9d4cfcc5fd19c
ibolton336 <ibolton@redhat.com>
2023-07-20 21:28:22 -0400

    Add unit tests for regex
    Signed-off-by: ibolton336 <ibolton@redhat.com>

--- client/src/app/utils/utils.test.ts
+++ client/src/app/utils/utils.test.ts
@@ -1,9 +1,14 @@
+import { act, render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
 import { AxiosError } from "axios";
 import {
   getAxiosErrorMessage,
   getValidatedFromError,
   getValidatedFromErrors,
   getToolbarChipKey,
+  customURLValidation,
+  gitUrlRegex,
+  standardURLRegex,
 } from "./utils";
 
 describe("utils", () => {
@@ -88,5 +93,66 @@ describe("utils", () => {
   it("getToolbarChipKey: test 'ToolbarChip'", () => {
     const result = getToolbarChipKey({ key: "myKey", node: "myNode" });
     expect(result).toBe("myKey");
+  });
+
+  //URL Regex tests
+  it("Regex should validate git URLs", () => {
+    const testGitURLs: string[] = [
+      "git@github.com:konveyor/tackle2-ui",
+      "http://git@github.com:konveyor/tackle2-ui",
+    ];
+
+    for (const url of testGitURLs) {
+      const gitTestResult = gitUrlRegex.test(url);
+      expect(gitTestResult).toBe(true);
+    }
+  });
+
+  it("Regex should validate standard URLs", () => {
+    const testStandardURLs: string[] = [
+      "http://www.foo.bar",
+      "www.foo.bar",
+      "https://www.github.com/ibolton336/tackle-testapp.git",
+    ];
+
+    for (const url of testStandardURLs) {
+      const standardTestResult = standardURLRegex.test(url);
+      expect(standardTestResult).toBe(true);
+    }
+  });
+
+  it("Regex should fail when validating broken standard URLs", () => {
+    const testBrokenURLs: string[] = [
+      "",
+      " http://www.foo.bar ",
+      " http://www.foo",
+      " http://wrong",
+      "wwwfoo.bar",
+      "foo.bar",
+      "www.foo.b",
+      "foo.ba",
+      "git@github.com:konveyor/tackle2-ui",
+    ];
+
+    for (const url of testBrokenURLs) {
+      const testResult = standardURLRegex.test(url);
+      expect(testResult).toBe(false);
+    }
+  });
+
+  it("URL should match the same multiple times in a row", () => {
+    // Motivation for this test:
+    // https://stackoverflow.com/a/21373261/1183614
+    // https://stackoverflow.com/a/1520853/1183614
+    // When using the global flag on a regex, subsequent results will return a result from the last used index.
+    // In sum, we should never be using a global flag in combination with the .test method.
+
+    const url = "https://github.com/ibolton336/tackle-testapp.git";
+
+    expect(standardURLRegex.test(url)).toBe(true);
+    expect(standardURLRegex.test(url)).toBe(true);
+    expect(standardURLRegex.test(url)).toBe(true);
+    expect(standardURLRegex.test(url)).toBe(true);
+    expect(standardURLRegex.test(url)).toBe(true);
   });
 });
