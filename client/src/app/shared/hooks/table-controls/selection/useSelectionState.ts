da747caaea2785a82a7fcdace24abf9263bf3a91
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-07-25 16:04:31 -0400

    TODOs
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

---
+++ client/src/app/shared/hooks/table-controls/selection/useSelectionState.ts
@@ -0,0 +1,4 @@
+// TODO move useSelectionState as-is into here, rename to useLegacySelectionState
+// TODO restructure useSelectionState and useSelectionUrlParams like the other concerns in table hooks - rely on idProperty, etc
+// TODO figure out how to make the URL param piece of it optional? give useUrlParams a disabled flag? maybe make that optional for all the concerns? play around with that...
+// TODO make useUrlParams just omit the param from the URL if it is using the default -- does this prevent all the history.replace thrashing on first page mount?
