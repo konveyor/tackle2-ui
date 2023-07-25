880d8a8113e1e8f4a3b17e2b1e6d7632f962e837
Gilles Dubreuil <gdubreui@redhat.com>
2023-07-25 16:46:22 +0200

    cleanup
    Signed-off-by: Gilles Dubreuil <gdubreui@redhat.com>

---
+++ client/src/app/pages/controls/job-functions/components/job-function-form.tsx
@@ -0,0 +1,167 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { AxiosError, AxiosResponse } from "axios";
+import { object, string } from "yup";
+
+import {
+  ActionGroup,
+  Button,
+  ButtonVariant,
+  Form,
+} from "@patternfly/react-core";
+
+import { JobFunction, New } from "@app/api/models";
+import { duplicateNameCheck } from "@app/utils/utils";
+import {
+  useCreateJobFunctionMutation,
+  useFetchJobFunctions,
+  useUpdateJobFunctionMutation,
+} from "@app/queries/jobfunctions";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { HookFormPFTextInput } from "@app/shared/components/hook-form-pf-fields";
+import { NotificationsContext } from "@app/shared/notifications-context";
+
+export interface FormValues {
+  name: string;
+}
+
+export interface JobFunctionFormProps {
+  jobFunction?: JobFunction;
+  onClose: () => void;
+}
+
+export const JobFunctionForm: React.FC<JobFunctionFormProps> = ({
+  jobFunction,
+  onClose,
+}) => {
+  const { t } = useTranslation();
+  const { pushNotification } = React.useContext(NotificationsContext);
+  const { jobFunctions } = useFetchJobFunctions();
+
+  const validationSchema = object().shape({
+    name: string()
+      .trim()
+      .required(t("validation.required"))
+      .min(3, t("validation.minLength", { length: 3 }))
+      .max(120, t("validation.maxLength", { length: 120 }))
+      .test(
+        "Duplicate name",
+        "A job function with this name already exists. Use a different name.",
+        (value) => {
+          return duplicateNameCheck(
+            jobFunctions || [],
+            jobFunction || null,
+            value || ""
+          );
+        }
+      ),
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting, isValidating, isValid, isDirty },
+    control,
+  } = useForm<FormValues>({
+    defaultValues: {
+      name: jobFunction?.name || "",
+    },
+    resolver: yupResolver(validationSchema),
+    mode: "onChange",
+  });
+
+  const onCreateJobFunctionSuccess = (response: AxiosResponse<JobFunction>) => {
+    pushNotification({
+      title: t("toastr.success.createWhat", {
+        type: t("terms.jobFunction"),
+        what: response.data.name,
+      }),
+      variant: "success",
+    });
+    onClose();
+  };
+
+  const onUpdateJobFunctionSuccess = (response: AxiosResponse<JobFunction>) => {
+    pushNotification({
+      title: t("toastr.success.save", {
+        type: t("terms.jobFunction"),
+      }),
+      variant: "success",
+    });
+    onClose();
+  };
+
+  const onCreateJobFunctionError = (error: AxiosError) => {
+    pushNotification({
+      title: t("toastr.fail.create", {
+        type: t("terms.jobFunction").toLowerCase(),
+      }),
+      variant: "danger",
+    });
+  };
+
+  const { mutate: createJobFunction } = useCreateJobFunctionMutation(
+    onCreateJobFunctionSuccess,
+    onCreateJobFunctionError
+  );
+
+  const onUpdateJobFunctionError = (error: AxiosError) => {
+    pushNotification({
+      title: t("toastr.fail.save", {
+        type: t("terms.jobFunction").toLowerCase(),
+      }),
+      variant: "danger",
+    });
+  };
+
+  const { mutate: updateJobFunction } = useUpdateJobFunctionMutation(
+    onUpdateJobFunctionSuccess,
+    onUpdateJobFunctionError
+  );
+
+  const onSubmit = (formValues: FormValues) => {
+    const payload: New<JobFunction> = {
+      name: formValues.name.trim(),
+    };
+
+    if (jobFunction) {
+      updateJobFunction({ id: jobFunction.id, ...payload });
+    } else {
+      createJobFunction(payload);
+    }
+    onClose();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <HookFormPFTextInput
+        control={control}
+        name="name"
+        label={t("terms.name")}
+        fieldId="name"
+        isRequired
+      />
+      <ActionGroup>
+        <Button
+          type="submit"
+          id="job-function-form-submit"
+          aria-label="submit"
+          variant={ButtonVariant.primary}
+          isDisabled={!isValid || isSubmitting || isValidating || !isDirty}
+        >
+          {!jobFunction ? t("actions.create") : t("actions.save")}
+        </Button>
+        <Button
+          type="button"
+          id="cancel"
+          aria-label="cancel"
+          variant={ButtonVariant.link}
+          isDisabled={isSubmitting || isValidating}
+          onClick={onClose}
+        >
+          {t("actions.cancel")}
+        </Button>
+      </ActionGroup>
+    </Form>
+  );
+};
