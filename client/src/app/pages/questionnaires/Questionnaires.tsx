72137e6c46a2447a42713fac2d4c777d728204ff
Gilles Dubreuil <gdubreui@redhat.com>
2023-08-09 17:43:09 +0200

    Remove unused selectedItems
    Signed-off-by: Gilles Dubreuil <gdubreui@redhat.com>

--- client/src/app/pages/questionnaires/Questionnaires.tsx
+++ client/src/app/pages/questionnaires/Questionnaires.tsx
@@ -170,7 +170,6 @@ export const Questionnaires: React.FC = () => {
   const {
     currentPageItems,
     numRenderedColumns,
-    selectionState: { selectedItems },
     propHelpers: {
       toolbarProps,
       filterToolbarProps,
