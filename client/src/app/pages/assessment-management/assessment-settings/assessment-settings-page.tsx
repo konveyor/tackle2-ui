cb9c67363c08771c2fb23ca626cdae92422fccec
Aviv Turgeman <aturgema@redhat.com>
2023-08-23 10:14:58 +0300

    Add lock icon for "system" questionnaires
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- client/src/app/pages/assessment-management/assessment-settings/assessment-settings-page.tsx
+++ client/src/app/pages/assessment-management/assessment-settings/assessment-settings-page.tsx
@@ -25,6 +25,7 @@ import {
 } from "@patternfly/react-core";
 import { AxiosError } from "axios";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@patternfly/react-table";
+import { LockIcon } from "@patternfly/react-icons";
 import CubesIcon from "@patternfly/react-icons/dist/esm/icons/cubes-icon";
 import EllipsisVIcon from "@patternfly/react-icons/dist/esm/icons/ellipsis-v-icon";
 
@@ -274,6 +275,14 @@ const AssessmentSettings: React.FC = () => {
                             />
                           </Td>
                           <Td width={25} {...getTdProps({ columnKey: "name" })}>
+                            {questionnaire.system && (
+                              <LockIcon
+                                style={{
+                                  marginRight:
+                                    "var(--pf-v5-global--spacer--sm)",
+                                }}
+                              />
+                            )}
                             {questionnaire.name}
                           </Td>
                           <Td
