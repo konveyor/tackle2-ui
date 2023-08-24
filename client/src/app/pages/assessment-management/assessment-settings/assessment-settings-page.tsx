dbb35a14785ff7f4bb46bbf0f2a2af089ac08986
Aviv Turgeman <aturgema@redhat.com>
2023-08-24 21:28:39 +0300

    :sparkles: Add lock icon for "system" questionnaires (#1302)
    closes https://github.com/konveyor/tackle2-ui/issues/1292
    
    Adding a lock icon to "system" questionnaire.
    
    Before:
    
    ![lock-icon-b4](https://github.com/konveyor/tackle2-ui/assets/67270715/ecb76fd5-ca93-494a-ae93-f65a991120a9)
    
    After:
    
    ![lock-icon](https://github.com/konveyor/tackle2-ui/assets/67270715/7756e178-bb36-41ac-86b6-25ef0a42f284)
    
    ---------
    
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>
    Co-authored-by: Ian Bolton <ibolton@redhat.com>

--- client/src/app/pages/assessment-management/assessment-settings/assessment-settings-page.tsx
+++ client/src/app/pages/assessment-management/assessment-settings/assessment-settings-page.tsx
@@ -25,8 +25,8 @@ import {
 } from "@patternfly/react-core";
 import { AxiosError } from "axios";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@patternfly/react-table";
-import CubesIcon from "@patternfly/react-icons/dist/esm/icons/cubes-icon";
-import EllipsisVIcon from "@patternfly/react-icons/dist/esm/icons/ellipsis-v-icon";
+import { LockIcon, EllipsisVIcon, CubesIcon } from "@patternfly/react-icons";
+import spacing from "@patternfly/react-styles/css/utilities/Spacing/spacing";
 
 import {
   useDeleteQuestionnaireMutation,
@@ -274,6 +274,9 @@ const AssessmentSettings: React.FC = () => {
                             />
                           </Td>
                           <Td width={25} {...getTdProps({ columnKey: "name" })}>
+                            {questionnaire.system && (
+                              <LockIcon className={spacing.mrSm} />
+                            )}
                             {questionnaire.name}
                           </Td>
                           <Td
