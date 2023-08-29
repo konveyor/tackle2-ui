ccbf1c8ed2aa439b5ba8313ebfec51c2d43b36e6
Gilles Dubreuil <gdubreui@redhat.com>
2023-08-29 16:55:19 +0200

    Use onChange() instead of setValue()
    Signed-off-by: Gilles Dubreuil <gdubreui@redhat.com>

--- client/src/app/pages/identities/components/identity-form/identity-form.tsx
+++ client/src/app/pages/identities/components/identity-form/identity-form.tsx
@@ -465,7 +465,7 @@ export const IdentityForm: React.FC<IdentityFormProps> = ({
         label="Type"
         fieldId="type-select"
         isRequired
-        renderInput={({ field: { value, name } }) => (
+        renderInput={({ field: { value, name, onChange } }) => (
           <SimpleSelect
             id="type-select"
             toggleId="type-select-toggle"
@@ -475,7 +475,7 @@ export const IdentityForm: React.FC<IdentityFormProps> = ({
             options={kindOptions}
             onChange={(selection) => {
               const selectionValue = selection as OptionWithValue<IdentityKind>;
-              setValue(name, selectionValue.value);
+              onChange(selectionValue.value);
               // So we don't retain the values from the wrong type of credential
               resetField("user");
               resetField("password");
@@ -485,14 +485,14 @@ export const IdentityForm: React.FC<IdentityFormProps> = ({
       />
 
       {values?.kind === "source" && (
-        <>
+        <div className="pf-v5-c-form">
           <HookFormPFGroupController
             control={control}
             name="userCredentials"
             label="User credentials"
             isRequired
             fieldId="user-credentials-select"
-            renderInput={({ field: { value, name } }) => (
+            renderInput={({ field: { value, name, onChange } }) => (
               <SimpleSelect
                 id="user-credentials-select"
                 toggleId="user-credentials-select-toggle"
@@ -507,7 +507,7 @@ export const IdentityForm: React.FC<IdentityFormProps> = ({
                 onChange={(selection) => {
                   const selectionValue =
                     selection as OptionWithValue<UserCredentials>;
-                  setValue(name, selectionValue.value);
+                  onChange(selectionValue.value);
                   // So we don't retain the values from the wrong type of credential
                   resetField("user");
                   resetField("password");
@@ -612,7 +612,7 @@ export const IdentityForm: React.FC<IdentityFormProps> = ({
               />
             </>
           )}
-        </>
+        </div>
       )}
 
       {values?.kind === "maven" && (
