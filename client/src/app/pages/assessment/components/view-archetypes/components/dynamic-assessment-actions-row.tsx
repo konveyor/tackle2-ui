8cbc2472fe3761f52b4bf6910a1692e758252a4a
ibolton336 <ibolton@redhat.com>
2023-09-20 15:50:00 -0400

    :sparkles: Add view archetypes page
    Signed-off-by: ibolton336 <ibolton@redhat.com>

---
+++ client/src/app/pages/assessment/components/view-archetypes/components/dynamic-assessment-actions-row.tsx
@@ -0,0 +1,273 @@
+import { Paths } from "@app/Paths";
+import {
+  Application,
+  Archetype,
+  Assessment,
+  InitialAssessment,
+  Questionnaire,
+} from "@app/api/models";
+import {
+  assessmentsByItemIdQueryKey,
+  useCreateAssessmentMutation,
+  useDeleteAssessmentMutation,
+} from "@app/queries/assessments";
+import { Button } from "@patternfly/react-core";
+import React, { FunctionComponent } from "react";
+import { useHistory } from "react-router-dom";
+import "./dynamic-assessment-actions-row.css";
+import { AxiosError } from "axios";
+import { formatPath, getAxiosErrorMessage } from "@app/utils/utils";
+import { Td } from "@patternfly/react-table";
+import { NotificationsContext } from "@app/components/NotificationsContext";
+import { useTranslation } from "react-i18next";
+import { useQueryClient } from "@tanstack/react-query";
+import { TrashIcon } from "@patternfly/react-icons";
+import useIsArchetype from "@app/hooks/useIsArchetype";
+
+enum AssessmentAction {
+  Take = "Take",
+  Retake = "Retake",
+  Continue = "Continue",
+}
+
+interface DynamicAssessmentActionsRowProps {
+  questionnaire: Questionnaire;
+  application?: Application;
+  archetype?: Archetype;
+  assessment?: Assessment;
+  isReadonly?: boolean;
+}
+
+const DynamicAssessmentActionsRow: FunctionComponent<
+  DynamicAssessmentActionsRowProps
+> = ({ questionnaire, application, archetype, assessment, isReadonly }) => {
+  const isArchetype = useIsArchetype();
+  const history = useHistory();
+  const { t } = useTranslation();
+  const queryClient = useQueryClient();
+
+  const { pushNotification } = React.useContext(NotificationsContext);
+
+  const onSuccessHandler = () => {};
+  const onErrorHandler = () => {};
+
+  const { mutateAsync: createAssessmentAsync } = useCreateAssessmentMutation(
+    isArchetype,
+    onSuccessHandler,
+    onErrorHandler
+  );
+
+  const onDeleteAssessmentSuccess = (name: string) => {
+    pushNotification({
+      title: t("toastr.success.assessmentDiscarded", {
+        application: name,
+      }),
+      variant: "success",
+    });
+    queryClient.invalidateQueries([assessmentsByItemIdQueryKey]);
+  };
+
+  const onDeleteError = (error: AxiosError) => {
+    pushNotification({
+      title: getAxiosErrorMessage(error),
+      variant: "danger",
+    });
+  };
+
+  const { mutate: deleteAssessment } = useDeleteAssessmentMutation(
+    onDeleteAssessmentSuccess,
+    onDeleteError
+  );
+
+  const { mutateAsync: deleteAssessmentAsync } = useDeleteAssessmentMutation(
+    onDeleteAssessmentSuccess,
+    onDeleteError
+  );
+
+  const determineAction = () => {
+    if (!assessment) {
+      return AssessmentAction.Take;
+    } else if (assessment.status === "started") {
+      return AssessmentAction.Continue;
+    } else {
+      return AssessmentAction.Retake;
+    }
+  };
+
+  const determineButtonClassName = () => {
+    const action = determineAction();
+    if (action === AssessmentAction.Continue) {
+      return "continue-button";
+    } else if (action === AssessmentAction.Retake) {
+      return "retake-button";
+    }
+  };
+
+  const createAssessment = async () => {
+    const newAssessment: InitialAssessment = {
+      questionnaire: { name: questionnaire.name, id: questionnaire.id },
+      ...(isArchetype
+        ? archetype
+          ? { archetype: { name: archetype.name, id: archetype.id } }
+          : {}
+        : application
+        ? { application: { name: application.name, id: application.id } }
+        : {}),
+    };
+
+    try {
+      const result = await createAssessmentAsync(newAssessment);
+      if (isArchetype) {
+        history.push(
+          formatPath(Paths.archetypesAssessment, {
+            assessmentId: result.id,
+          })
+        );
+      } else {
+        history.push(
+          formatPath(Paths.applicationsAssessment, {
+            assessmentId: result.id,
+          })
+        );
+      }
+    } catch (error) {
+      console.error("Error while creating assessment:", error);
+      pushNotification({
+        title: t("terms.error"),
+        variant: "danger",
+      });
+    }
+  };
+
+  const takeAssessment = async () => {
+    if (!isArchetype && application?.archetypes?.length) {
+      for (const archetypeRef of application.archetypes) {
+        try {
+          createAssessment();
+        } catch (error) {
+          console.error(
+            `Error fetching archetype with ID ${archetypeRef.id}:`,
+            error
+          );
+          pushNotification({
+            title: t("terms.error"),
+            variant: "danger",
+          });
+        }
+      }
+    } else {
+      createAssessment();
+    }
+  };
+
+  const onHandleAssessmentAction = async () => {
+    const action = determineAction();
+
+    if (action === AssessmentAction.Take) {
+      takeAssessment();
+    } else if (action === AssessmentAction.Continue) {
+      history.push(
+        formatPath(
+          isArchetype
+            ? Paths.archetypesAssessment
+            : Paths.applicationsAssessment,
+          {
+            assessmentId: assessment?.id,
+          }
+        )
+      );
+    } else if (action === AssessmentAction.Retake) {
+      if (assessment) {
+        try {
+          await deleteAssessmentAsync({
+            name: assessment.name,
+            id: assessment.id,
+          }).then(() => {
+            createAssessment();
+          });
+          history.push(
+            formatPath(
+              isArchetype
+                ? Paths.archetypesAssessment
+                : Paths.applicationsAssessment,
+              {
+                assessmentId: assessment?.id,
+              }
+            )
+          );
+        } catch (error) {
+          pushNotification({
+            title: t("terms.error"),
+            variant: "danger",
+          });
+          console.error("Error while deleting assessment:", error);
+        }
+      }
+    }
+  };
+
+  const viewButtonLabel = "View";
+
+  return (
+    <>
+      <Td>
+        <div>
+          {!isReadonly ? (
+            <Button
+              type="button"
+              variant="primary"
+              className={determineButtonClassName()}
+              onClick={() => {
+                onHandleAssessmentAction();
+              }}
+            >
+              {determineAction()}
+            </Button>
+          ) : null}
+          {assessment?.status === "complete" ? (
+            <Button
+              type="button"
+              variant="secondary"
+              onClick={() => {
+                history.push(
+                  formatPath(
+                    isArchetype
+                      ? Paths.archetypeAssessmentSummary
+                      : Paths.applicationAssessmentSummary,
+                    {
+                      assessmentId: assessment.id,
+                    }
+                  )
+                );
+              }}
+            >
+              {viewButtonLabel}
+            </Button>
+          ) : isReadonly ? (
+            <Button type="button" variant="secondary" isDisabled={true}>
+              {viewButtonLabel}
+            </Button>
+          ) : null}
+        </div>
+      </Td>
+      {assessment ? (
+        <Td isActionCell>
+          <Button
+            type="button"
+            variant="plain"
+            onClick={() => {
+              deleteAssessment({
+                id: assessment.id,
+                name: assessment.name,
+              });
+            }}
+          >
+            <TrashIcon />
+          </Button>
+        </Td>
+      ) : null}
+    </>
+  );
+};
+
+export default DynamicAssessmentActionsRow;
---
+++ client/src/app/pages/assessment/components/view-archetypes/components/questionnaires-table.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@patternfly/react-table";
+
+import { useLocalTableControls } from "@app/hooks/table-controls";
+import {
+  ConditionalTableBody,
+  TableHeaderContentWithControls,
+  TableRowContentWithControls,
+} from "@app/components/TableControls";
+import { NoDataEmptyState } from "@app/components/NoDataEmptyState";
+import {
+  Application,
+  Archetype,
+  Assessment,
+  Questionnaire,
+} from "@app/api/models";
+import DynamicAssessmentActionsRow from "./dynamic-assessment-actions-row";
+
+interface QuestionnairesTableProps {
+  tableName: string;
+  isFetching: boolean;
+  isReadonly?: boolean;
+  application?: Application;
+  archetype?: Archetype;
+  assessments?: Assessment[];
+  questionnaires?: Questionnaire[];
+}
+
+const QuestionnairesTable: React.FC<QuestionnairesTableProps> = ({
+  assessments,
+  questionnaires,
+  isReadonly,
+  application,
+  archetype,
+  tableName,
+}) => {
+  const tableControls = useLocalTableControls({
+    idProperty: "id",
+    items: questionnaires || [],
+    columnNames: {
+      questionnaires: tableName,
+    },
+    hasActionsColumn: false,
+    hasPagination: false,
+    variant: "compact",
+  });
+
+  const {
+    currentPageItems,
+    numRenderedColumns,
+    propHelpers: { tableProps, getThProps, getTdProps },
+  } = tableControls;
+  return (
+    <>
+      <Table
+        {...tableProps}
+        aria-label={`Questionnaire table for assessment actions`}
+        style={{ background: "none" }}
+      >
+        <Thead>
+          <Tr>
+            <TableHeaderContentWithControls {...tableControls}>
+              <Th {...getThProps({ columnKey: "questionnaires" })} />
+            </TableHeaderContentWithControls>
+          </Tr>
+        </Thead>
+        <ConditionalTableBody
+          isNoData={questionnaires?.length === 0}
+          numRenderedColumns={numRenderedColumns}
+          noDataEmptyState={
+            <div>
+              <NoDataEmptyState title="No Questionnaires are currently available to be taken. " />
+            </div>
+          }
+        >
+          <Tbody>
+            {currentPageItems?.map((questionnaire, rowIndex) => {
+              const matchingArchetypeAssessment = assessments?.find(
+                (assessment) =>
+                  assessment.questionnaire.id === questionnaire.id &&
+                  assessment?.archetype?.id === archetype?.id
+              );
+
+              const matchingApplicationAssessment = assessments?.find(
+                (assessment) =>
+                  assessment.questionnaire.id === questionnaire.id &&
+                  assessment?.application?.id === application?.id
+              );
+
+              return (
+                <Tr key={questionnaire.name}>
+                  <TableRowContentWithControls
+                    {...tableControls}
+                    item={questionnaire}
+                    rowIndex={rowIndex}
+                  >
+                    <Td
+                      width={20}
+                      {...getTdProps({ columnKey: "questionnaires" })}
+                    >
+                      {questionnaire.name}
+                    </Td>
+                    {application || archetype ? (
+                      <DynamicAssessmentActionsRow
+                        assessment={
+                          archetype
+                            ? matchingArchetypeAssessment
+                            : matchingApplicationAssessment
+                        }
+                        questionnaire={questionnaire}
+                        application={application}
+                        archetype={archetype}
+                        isReadonly
+                      />
+                    ) : null}
+                  </TableRowContentWithControls>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </ConditionalTableBody>
+      </Table>
+    </>
+  );
+};
+
+export default QuestionnairesTable;
