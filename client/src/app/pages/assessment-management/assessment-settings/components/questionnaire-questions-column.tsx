65eeaebd0b1c094975aba9e163176a3e22b16a88
ibolton336 <ibolton@redhat.com>
2023-09-22 13:25:20 -0400

    :bug: Implement assessment settings columns
    Signed-off-by: ibolton336 <ibolton@redhat.com>

---
+++ client/src/app/pages/assessment-management/assessment-settings/components/questionnaire-questions-column.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "@patternfly/react-core";
+import { Questionnaire } from "@app/api/models";
+
+export const QuestionnaireQuestionsColumn: React.FC<{
+  questionnaire: Questionnaire;
+}> = ({ questionnaire }) => {
+  const totalQuestions = (questionnaire.sections || []).reduce(
+    (total, section) => {
+      return total + (section.questions ? section.questions.length : 0);
+    },
+    0
+  );
+  return <Text>{totalQuestions}</Text>;
+};
