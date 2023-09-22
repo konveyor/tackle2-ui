b97b11a6d994dee54605252f185afbc8197edd7e
Scott J Dickerson <sdickers@redhat.com>
2023-09-22 16:53:46 -0400

    :sparkles: Reports - Current Landscape update risk handling
    Custom assessments changes risk and confidence to be accessed from
    `Assessment` and not `Application`.  The reports page in general, and
    the `Landscape` component in specific need to be updated to use risk
    values from `Assessment`.
    
    Summary of Change:
      - `Reports`
        - Fetch `Questionnaires` and `Assessments` to allow easy use
          in the Questionnaire select menu
        - Adjust the `Card`s to be clickable and selectable when we provide
          custom actions (to avoid a console warning)
    
      - `Landscape`
        - Use data from props instead of fetching any additional data. The
          containing component is now responsible for controlling the
          source data.
        - Aggregate risk data into buckets matching `Risk` options
        - Setup responsive layout so the donut charts wrap nicely when the
          view becomes narrow
    
      - `Donut`
        - Force `id` to be provided
        - Set the width to `200px`
        - Make sure content lines up centered in its container
    
      - Deprecated `useFetchRisks()`
    
    Enhancement: https://github.com/konveyor/enhancements/blob/90b827b68cc367284a66bf66f087d5c263487e05/enhancements/assessment-module/README.md#changes-in-the-application-reports-view
    Part Of: #1305
    Follow Up: #1374
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/queries/risks.ts
+++ client/src/app/queries/risks.ts
@@ -3,20 +3,20 @@ import { AssessmentRisk } from "@app/api/models";
 
 export const RisksQueryKey = "risks";
 
+/** @deprecated Risk is attached to assessments now. */
 export const useFetchRisks = (applicationIDs: number[]) => {
-  const { data, refetch, isFetching, error } = useQuery<AssessmentRisk[]>(
-    ["assessmentrisks", applicationIDs],
-    async () => {
+  const { data, refetch, isFetching, error } = useQuery<AssessmentRisk[]>({
+    queryKey: ["assessmentrisks", applicationIDs],
+    queryFn: async () => {
       if (applicationIDs.length > 0)
         // return (await getAssessmentLandscape(applicationIDs)).data;
         //TODO see if we still need this
         return [];
       else return [];
     },
-    {
-      onError: (error) => console.log("error, ", error),
-    }
-  );
+    onError: (error) => console.log("error, ", error),
+  });
+
   return {
     risks: data || [],
     isFetching,
