f3a5e6ae15f61a618becf2f9a0b08d4e002eb6d2
Dallas <dallas.nicol@gmail.com>
2023-10-04 18:38:40 -0500

    :seedling: fix childpaths to work with nested nodemodules (#1421)
    this came up while testing prerelease pf versions
    
    @sjd78 came up with these changes to be more flexible with where
    stylepaths can be
    
    ---------
    
    Signed-off-by: gitdallas <dallas.nicol@gmail.com>
    Co-authored-by: Scott Dickerson <sdickers@redhat.com>

--- client/config/stylePaths.ts
+++ client/config/stylePaths.ts
@@ -3,28 +3,15 @@
 import * as path from "path";
 
 export const stylePaths = [
+  // Include our sources
   path.resolve(__dirname, "../src"),
+
+  // Include <PF4 paths
   path.resolve(__dirname, "../../node_modules/patternfly"),
-  path.resolve(__dirname, "../../node_modules/@patternfly/patternfly"),
-  path.resolve(__dirname, "../../node_modules/@patternfly/react-styles/css"),
-  path.resolve(
-    __dirname,
-    "../../node_modules/@patternfly/react-core/dist/styles/base.css"
-  ),
-  path.resolve(
-    __dirname,
-    "../../node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly"
-  ),
-  path.resolve(
-    __dirname,
-    "../../node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css"
-  ),
-  path.resolve(
-    __dirname,
-    "../../node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css"
-  ),
-  path.resolve(
-    __dirname,
-    "../../node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css"
-  ),
+
+  // Include >=PF4 paths, even if nested under another package because npm cannot hoist
+  // a single package to the root node_modules/
+  /node_modules\/@patternfly\/patternfly/,
+  /node_modules\/@patternfly\/react-core\/.*\.css/,
+  /node_modules\/@patternfly\/react-styles/,
 ];
