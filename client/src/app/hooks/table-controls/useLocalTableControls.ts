a4649bf50649bb9e12335fdcd8b93687a503b6f7
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-10-10 13:46:28 -0400

    Fix missing type params in useLocalTableControls
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/hooks/table-controls/useLocalTableControls.ts
+++ client/src/app/hooks/table-controls/useLocalTableControls.ts
@@ -6,6 +6,14 @@ export const useLocalTableControls = <
   TItem,
   TColumnKey extends string,
   TSortableColumnKey extends TColumnKey,
+  TFilterCategoryKey extends string = string,
+  TPersistenceKeyPrefix extends string = string,
 >(
-  args: IUseLocalTableControlStateArgs<TItem, TColumnKey, TSortableColumnKey>
+  args: IUseLocalTableControlStateArgs<
+    TItem,
+    TColumnKey,
+    TSortableColumnKey,
+    TFilterCategoryKey,
+    TPersistenceKeyPrefix
+  >
 ) => useTableControlProps(useLocalTableControlState(args));
