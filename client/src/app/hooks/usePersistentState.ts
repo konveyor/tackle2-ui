6f4c8a4c2307a1ab9be029a9d751ffcb1c7e095c
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-10-10 16:17:54 -0400

    Fix fallback behavior in usePersistentState
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/hooks/usePersistentState.ts
+++ client/src/app/hooks/usePersistentState.ts
@@ -16,21 +16,29 @@ export type BaseUsePersistentStateOptions<
   persistenceKeyPrefix?: DisallowCharacters<TPersistenceKeyPrefix, ":">;
 };
 
+type PersistToStateOptions = { persistTo?: "state" };
+
+type PersistToUrlParamsOptions<
+  TValue,
+  TPersistenceKeyPrefix extends string,
+  TURLParamKey extends string,
+> = {
+  persistTo: "urlParams";
+} & IUseUrlParamsArgs<TValue, TPersistenceKeyPrefix, TURLParamKey>;
+
+type PersistToStorageOptions<TValue> = {
+  persistTo: "localStorage" | "sessionStorage";
+} & UseStorageTypeOptions<TValue>;
+
 export type UsePersistentStateOptions<
   TValue,
   TPersistenceKeyPrefix extends string,
   TURLParamKey extends string,
 > = BaseUsePersistentStateOptions<TValue> &
   (
-    | {
-        persistTo?: "state";
-      }
-    | ({
-        persistTo: "urlParams";
-      } & IUseUrlParamsArgs<TValue, TPersistenceKeyPrefix, TURLParamKey>)
-    | ({
-        persistTo: "localStorage" | "sessionStorage";
-      } & UseStorageTypeOptions<TValue>)
+    | PersistToStateOptions
+    | PersistToUrlParamsOptions<TValue, TPersistenceKeyPrefix, TURLParamKey>
+    | PersistToStorageOptions<TValue>
   );
 
 export const usePersistentState = <
@@ -44,37 +52,47 @@ export const usePersistentState = <
     TURLParamKey
   >
 ): [TValue, (value: TValue) => void] => {
-  const {
-    defaultValue,
-    isEnabled = true,
-    persistTo,
-    persistenceKeyPrefix,
-  } = options;
-  const urlParamOptions = options as IUseUrlParamsArgs<
+  const { defaultValue, persistTo, persistenceKeyPrefix } = options;
+
+  const isUrlParamsOptions = (
+    o: typeof options
+  ): o is PersistToUrlParamsOptions<
     TValue,
     TPersistenceKeyPrefix,
     TURLParamKey
-  >;
-  const storageOptions = options as UseStorageTypeOptions<TValue>;
-  const prefixedStorageKey = persistenceKeyPrefix
-    ? `${persistenceKeyPrefix}:${storageOptions.key}`
-    : storageOptions.key;
+  > => o.persistTo === "urlParams";
+
+  const isStorageOptions = (
+    o: typeof options
+  ): o is PersistToStorageOptions<TValue> =>
+    o.persistTo === "localStorage" || o.persistTo === "sessionStorage";
+
+  const prefixKey = (key: string) =>
+    persistenceKeyPrefix ? `${persistenceKeyPrefix}:${key}` : key;
+
   const persistence = {
     state: React.useState(defaultValue),
-    urlParams: useUrlParams({
-      ...urlParamOptions,
-      isEnabled: isEnabled && persistTo === "urlParams",
-    }),
-    localStorage: useLocalStorage({
-      ...storageOptions,
-      isEnabled: isEnabled && persistTo === "localStorage",
-      key: prefixedStorageKey,
-    }),
-    sessionStorage: useSessionStorage({
-      ...(options as UseStorageTypeOptions<TValue>),
-      isEnabled: isEnabled && persistTo === "sessionStorage",
-      key: prefixedStorageKey,
-    }),
+    urlParams: useUrlParams(
+      isUrlParamsOptions(options)
+        ? options
+        : {
+            ...options,
+            isEnabled: false,
+            keys: [],
+            serialize: () => ({}),
+            deserialize: () => defaultValue,
+          }
+    ),
+    localStorage: useLocalStorage(
+      isStorageOptions(options)
+        ? { ...options, key: prefixKey(options.key) }
+        : { ...options, isEnabled: false, key: "" }
+    ),
+    sessionStorage: useSessionStorage(
+      isStorageOptions(options)
+        ? { ...options, key: prefixKey(options.key) }
+        : { ...options, isEnabled: false, key: "" }
+    ),
   };
   return persistence[persistTo || "state"];
 };
