d030a3d4b750c75e67a2ba7a973b9c299fd35b22
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-10-16 13:53:29 -0400

    Revert "Attempting to discriminate args for feature prop helpers"
    This reverts commit 8f4f4a59bb36b7a7ea944ea4e3f72277323f9a58.
    
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/hooks/table-controls/getLocalTableControlDerivedState.ts
+++ client/src/app/hooks/table-controls/getLocalTableControlDerivedState.ts
@@ -31,7 +31,7 @@ export const getLocalTableControlDerivedState = <
   const { items, isPaginationEnabled = true } = args;
   const { filteredItems } = getLocalFilterDerivedState({
     ...args,
-    ...(args.isFilterEnabled && { items }),
+    items,
   });
   const { sortedItems } = getLocalSortDerivedState({
     ...args,
