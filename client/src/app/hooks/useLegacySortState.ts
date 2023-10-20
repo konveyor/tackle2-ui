9b471a1df1240cbca9da34da7c5499e015b13103
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-10-20 14:23:53 -0400

    JSDocs for legacy filter/pagination/sort hooks
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- client/src/app/hooks/useLegacySortState.ts
+++ client/src/app/hooks/useLegacySortState.ts
@@ -2,15 +2,10 @@ import * as React from "react";
 import { ISortBy, SortByDirection } from "@patternfly/react-table";
 import i18n from "@app/i18n";
 
-// NOTE: This was refactored to expose an API based on columnKey instead of column index,
-//       and to return generic state data and decouple the client-side-sorting piece to another helper function.
-//       See useSortState for the new version, which should probably be used instead of this everywhere eventually.
-//       See useLocalSortDerivedState and getSortProps for the parts that were removed here.
-
-// NOTE ALSO: useLegacyFilterState and useLegacyPagination state were able to have their logic factored out
-//            to reuse the new helpers, but useLegacySortState has to retain its incompatible logic because of
-//            the switch from using column indexes to columnKeys.
-
+/**
+ * @deprecated The return value of useLegacySortState which predates table-controls/table-batteries and is deprecated.
+ * @see useLegacySortState
+ */
 export interface ILegacySortStateHook<T> {
   sortBy: ISortBy;
   onSort: (
@@ -21,6 +16,18 @@ export interface ILegacySortStateHook<T> {
   sortedItems: T[];
 }
 
+/**
+ * @deprecated This hook predates table-controls/table-batteries and still hasn't been refactored away from all of our tables.
+ *   It was refactored to expose an API based on columnKey instead of column index, and to return generic state data and decouple
+ *   the client-side-sorting piece to another helper function.
+ *   See useSortState in table-controls for the new version, which should probably be used instead of this everywhere eventually.
+ *   NOTE ALSO: useLegacyFilterState and useLegacyPagination state were able to have their logic factored out
+ *     to reuse the new helpers, but useLegacySortState has to retain its incompatible logic because of
+ *     the switch from using column indexes to `columnKey`s.
+ * @see useSortState
+ * @see getLocalSortDerivedState
+ * @see getSortProps
+ */
 export const useLegacySortState = <T>(
   items: T[],
   getSortValues?: (item: T) => (string | number | boolean)[],
