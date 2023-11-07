084f4076dd436206842ef1d36b72dcb646e32ee3
Dallas <dallas.nicol@gmail.com>
2023-11-07 09:30:52 -0600

    :ghost: use new select for set-mode, updates to simpleselectbasic (#1501)
    progress towards #1242
    
    made some updates to simpleselectbasic while getting it to work with
    set-mode.
    
    ---------
    
    Signed-off-by: gitdallas <dallas.nicol@gmail.com>
    Signed-off-by: Dallas <dallas.nicol@gmail.com>

--- client/src/app/components/SimpleSelectBasic.tsx
+++ client/src/app/components/SimpleSelectBasic.tsx
@@ -4,15 +4,19 @@ import {
   Select,
   SelectList,
   SelectOption,
+  SelectOptionProps,
   MenuToggle,
   MenuToggleElement,
 } from "@patternfly/react-core";
 
 export interface ISimpleSelectBasicProps {
   onChange: (selection: string) => void;
-  options: string[];
+  options: SelectOptionProps[];
   value?: string;
   placeholderText?: string;
+  selectId?: string;
+  toggleId?: string;
+  toggleAriaLabel?: string;
 }
 
 export const SimpleSelectBasic: React.FC<ISimpleSelectBasicProps> = ({
@@ -20,11 +24,16 @@ export const SimpleSelectBasic: React.FC<ISimpleSelectBasicProps> = ({
   options,
   value,
   placeholderText = "Select...",
+  selectId,
+  toggleId,
+  toggleAriaLabel,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = (toggleRef: React.Ref<MenuToggleElement>) => (
     <MenuToggle
+      aria-label={toggleAriaLabel}
+      id={toggleId}
       isFullWidth
       ref={toggleRef}
       onClick={() => {
@@ -32,12 +41,15 @@ export const SimpleSelectBasic: React.FC<ISimpleSelectBasicProps> = ({
       }}
       isExpanded={isOpen}
     >
-      {value || placeholderText || ""}
+      {(value && (options.find((o) => o.value === value)?.children || value)) ||
+        placeholderText ||
+        ""}
     </MenuToggle>
   );
 
   return (
     <Select
+      id={selectId}
       isOpen={isOpen}
       toggle={toggle}
       onOpenChange={(isOpen) => setIsOpen(isOpen)}
@@ -48,10 +60,12 @@ export const SimpleSelectBasic: React.FC<ISimpleSelectBasicProps> = ({
       }}
     >
       <SelectList>
-        {options.map((option, index) => (
-          <SelectOption key={`${index}-${option.toString()}`} value={option}>
-            {option.toString()}
-          </SelectOption>
+        {options.map((o: SelectOptionProps, index) => (
+          <SelectOption
+            key={`${index}-${o.value}`}
+            isSelected={o.value == value}
+            {...o}
+          />
         ))}
       </SelectList>
     </Select>
