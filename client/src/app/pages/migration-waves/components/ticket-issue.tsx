7fecfd9f9dcccf376b8adc7bee243abb8c575f10
ibolton336 <ibolton@redhat.com>
2023-11-22 16:02:58 -0500

    Move unlink and add external link usage
    Signed-off-by: ibolton336 <ibolton@redhat.com>

--- client/src/app/pages/migration-waves/components/ticket-issue.tsx
+++ client/src/app/pages/migration-waves/components/ticket-issue.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Button, Text, TextVariants } from "@patternfly/react-core";
+import { Text, TextVariants } from "@patternfly/react-core";
 
 import { Ticket } from "@app/api/models";
 import { useTranslation } from "react-i18next";
-import { UnlinkIcon } from "@patternfly/react-icons";
-import { useDeleteTicketMutation } from "@app/queries/migration-waves";
+import ExternalLink from "@app/components/ExternalLink";
 
 export interface ITicketIssueProps {
   ticket?: Ticket;
@@ -12,27 +11,14 @@ export interface ITicketIssueProps {
 
 export const TicketIssue: React.FC<ITicketIssueProps> = ({ ticket }) => {
   const { t } = useTranslation();
-  const { mutate: deleteTicket } = useDeleteTicketMutation();
 
   return (
     <>
-      <Text
-        component={TextVariants.p}
-        className="pf-v5-u-color-200 pf-v5-u-font-weight-light"
-      >
-        {ticket ? (
-          <a href={ticket.link} target="_">
-            {ticket?.link}
-          </a>
+      <Text component={TextVariants.p}>
+        {ticket?.link ? (
+          <ExternalLink href={ticket.link}>{ticket?.link}</ExternalLink>
         ) : (
           t("terms.unassigned")
-        )}
-        {ticket?.id && (
-          <Button
-            variant="link"
-            icon={<UnlinkIcon />}
-            onClick={() => deleteTicket(ticket.id)}
-          />
         )}
       </Text>
     </>
