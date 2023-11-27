77ab1a375c9abb07416716b2382ec526cc72bb3c
ibolton336 <ibolton@redhat.com>
2023-11-27 10:54:45 -0500

    Clean up ticket issue
    Signed-off-by: ibolton336 <ibolton@redhat.com>

--- client/src/app/pages/migration-waves/components/ticket-issue.tsx
+++ client/src/app/pages/migration-waves/components/ticket-issue.tsx
@@ -4,6 +4,7 @@ import { Text, TextVariants } from "@patternfly/react-core";
 import { Ticket } from "@app/api/models";
 import { useTranslation } from "react-i18next";
 import ExternalLink from "@app/components/ExternalLink";
+import { useTrackerTypesByProjectId } from "@app/queries/trackers";
 
 export interface ITicketIssueProps {
   ticket?: Ticket;
@@ -11,16 +12,25 @@ export interface ITicketIssueProps {
 
 export const TicketIssue: React.FC<ITicketIssueProps> = ({ ticket }) => {
   const { t } = useTranslation();
+  const ticketIssue = useTicketIssue(ticket);
 
   return (
-    <>
-      <Text component={TextVariants.p}>
-        {ticket?.link ? (
-          <ExternalLink href={ticket.link}>{ticket?.link}</ExternalLink>
-        ) : (
-          t("terms.unassigned")
-        )}
-      </Text>
-    </>
+    <Text component={TextVariants.p}>
+      {ticket?.link ? (
+        <ExternalLink href={ticket.link}>{ticketIssue}</ExternalLink>
+      ) : (
+        t("terms.unassigned")
+      )}
+    </Text>
   );
+};
+
+const useTicketIssue = (ticket?: Ticket) => {
+  const types = useTrackerTypesByProjectId(
+    ticket?.tracker?.name,
+    ticket?.parent
+  );
+  const type = types.find((kind) => kind.id === ticket?.kind);
+
+  return type ? type.name : "";
 };
