0d4810141a100e6b87113ed8ad1424b566d1de55
Fabian von Feilitzsch <fabian@fabianism.us>
2023-12-13 10:43:13 -0500

    Revert ":bug: Fix session expiry when refresh tokens are disabled (#1614)"
    This reverts commit eea7835f9033bf105e744d73456787b21d5fcc03.
    
    Signed-off-by: Fabian von Feilitzsch <fabian@fabianism.us>

--- client/src/app/axios-config/apiInit.ts
+++ client/src/app/axios-config/apiInit.ts
@@ -1,43 +1,14 @@
 import axios from "axios";
-import keycloak from "@app/keycloak";
 
-export const initInterceptors = () => {
+export const initInterceptors = (getToken: () => Promise<string>) => {
   axios.interceptors.request.use(
-    (config) => {
-      const token = keycloak.token;
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
+    async (config) => {
+      const token = await getToken();
+      if (token) config.headers["Authorization"] = "Bearer " + token;
       return config;
     },
     (error) => {
-      return Promise.reject(error);
-    }
-  );
-
-  axios.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    async (error) => {
-      if (error.response && error.response.status === 401) {
-        try {
-          const refreshed = await keycloak.updateToken(5);
-          if (refreshed) {
-            const retryConfig = {
-              ...error.config,
-              headers: {
-                ...error.config.headers,
-                Authorization: `Bearer ${keycloak.token}`,
-              },
-            };
-            return axios(retryConfig);
-          }
-        } catch (refreshError) {
-          keycloak.login();
-        }
-      }
-      return Promise.reject(error);
+      Promise.reject(error);
     }
   );
 };
