00c7e6a47bf84460f90427dc083e09d740826c12
Scott Dickerson <sdickers@redhat.com>
2023-12-13 12:01:55 -0500

    :bug: Only load keycloak when `AUTH_REQUIRED` is "true" (#1616)
    Only add the actual `AuthEnabledKeycloakProvider` keycloak handler if
    the `AUTH_REQUIRED` environment variable is set `"true"`.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/components/KeycloakProvider.tsx
+++ client/src/app/components/KeycloakProvider.tsx
@@ -3,12 +3,23 @@ import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "@app/keycloak";
 import { AppPlaceholder } from "./AppPlaceholder";
 import { initInterceptors } from "@app/axios-config";
+import ENV from "@app/env";
 
 interface IKeycloakProviderProps {
   children: React.ReactNode;
 }
 
 export const KeycloakProvider: React.FC<IKeycloakProviderProps> = ({
+  children,
+}) => {
+  return ENV.AUTH_REQUIRED !== "true" ? (
+    <>{children}</>
+  ) : (
+    <AuthEnabledKeycloakProvider>{children}</AuthEnabledKeycloakProvider>
+  );
+};
+
+const AuthEnabledKeycloakProvider: React.FC<IKeycloakProviderProps> = ({
   children,
 }) => {
   React.useEffect(() => {
