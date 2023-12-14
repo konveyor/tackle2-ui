b654645c56e8d0f66f41d9deb78dc115c2e18d0e
Scott Dickerson <sdickers@redhat.com>
2023-12-14 12:50:02 -0500

    :seedling: Refactor `WithUiId` handling to use hook `useWithUiId()` (#1555)
    Following up on #1554, create hook `useWithUiId()` to handle injecting
    UI id to objects. Any object `T` going will to the hook will come out as
    a `WithUiId<T>` object.
    
    Tables using the UI id have been adjusted to use the Constant
    `UI_UNIQUE_ID` as the `WithUiId<T>` table data `idProperty`.
    
    All uses of `WithUiId<T>` are now handled the same way.
    
    ---------
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/queries/dependencies.ts
+++ client/src/app/queries/dependencies.ts
@@ -1,70 +1,46 @@
-import { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import {
-  AnalysisAppDependency,
   AnalysisDependency,
   HubPaginatedResult,
   HubRequestParams,
   WithUiId,
 } from "@app/api/models";
 import { getAppDependencies, getDependencies } from "@app/api/rest";
-
-export interface IDependenciesFetchState {
-  result: HubPaginatedResult<WithUiId<AnalysisDependency>>;
-  isFetching: boolean;
-  fetchError: unknown;
-  refetch: () => void;
-}
-export interface IAppDependenciesFetchState {
-  result: HubPaginatedResult<AnalysisAppDependency>;
-  isFetching: boolean;
-  fetchError: unknown;
-  refetch: () => void;
-}
+import { useWithUiId } from "@app/utils/query-utils";
 
 export const DependenciesQueryKey = "dependencies";
 export const AppDependenciesQueryKey = "appDependencies";
 
-export const useFetchDependencies = (
-  params: HubRequestParams = {}
-): IDependenciesFetchState => {
-  const { data, isLoading, error, refetch } = useQuery({
+export const useFetchDependencies = (params: HubRequestParams = {}) => {
+  const {
+    data: dependencies,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery({
     queryKey: [DependenciesQueryKey, params],
     queryFn: async () => await getDependencies(params),
     onError: (error) => console.log("error, ", error),
     keepPreviousData: true,
   });
 
-  const result = useMemo(() => {
-    if (!data) {
-      return { data: [], total: 0, params };
-    }
-
-    const syntheticData: WithUiId<AnalysisDependency>[] = data.data.map(
-      (dep) => ({
-        ...dep,
-        _ui_unique_id: `${dep.name}/${dep.provider}`,
-      })
-    );
-
-    return {
-      data: syntheticData,
-      total: data.total,
-      params: data.params,
-    };
-  }, [data, params]);
-
+  const withUiId = useWithUiId(
+    dependencies?.data,
+    (d) => `${d.name}/${d.provider}`
+  );
   return {
-    result,
+    result: {
+      data: withUiId,
+      total: dependencies?.total ?? 0,
+      params: dependencies?.params ?? params,
+    } as HubPaginatedResult<WithUiId<AnalysisDependency>>,
     isFetching: isLoading,
     fetchError: error,
     refetch,
   };
 };
 
-export const useFetchAppDependencies = (
-  params: HubRequestParams = {}
-): IAppDependenciesFetchState => {
+export const useFetchAppDependencies = (params: HubRequestParams = {}) => {
   const { data, isLoading, error, refetch } = useQuery({
     queryKey: [AppDependenciesQueryKey, params],
     queryFn: async () => await getAppDependencies(params),
