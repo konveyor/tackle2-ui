edaa54b400b74f12fcead60871b5ab725a388fd2
Ian Bolton <ibolton@redhat.com>
2024-02-28 11:48:34 -0500

    Refactor status to reflect archived assessments
    Signed-off-by: Ian Bolton <ibolton@redhat.com>

--- client/src/app/pages/applications/components/application-assessment-status/application-assessment-status.tsx
+++ client/src/app/pages/applications/components/application-assessment-status/application-assessment-status.tsx
@@ -16,24 +16,73 @@ export const ApplicationAssessmentStatus: React.FC<
 > = ({ application }) => {
   const { t } = useTranslation();
 
-  const { archetypes, isFetching } = useFetchArchetypes();
+  const { archetypes, isFetching } = useFetchArchetypes(application);
   const { assessments, fetchError } = useFetchAssessments();
 
   const filteredAssessments = assessments?.filter(
     (assessment: Assessment) => assessment.application?.id === application.id
   );
+  const assessmentStatusInfo = React.useMemo(() => {
+    const assessmentsWithArchetypes = archetypes.map((archetype) => ({
+      archetype,
+      assessments: assessments.filter(
+        (assessment) => assessment.archetype?.id === archetype.id
+      ),
+    }));
 
-  const applicationArchetypes = application.archetypes?.map((archetypeRef) => {
-    return archetypes?.find((archetype) => archetype.id === archetypeRef.id);
-  });
+    const someArchetypesAssessed = assessmentsWithArchetypes.some(
+      ({ assessments }) => assessments.length > 0
+    );
 
-  const someArchetypesAssessed = applicationArchetypes?.some(
-    (archetype) => !!archetype?.assessments?.length ?? 0 > 0
-  );
-  const areAllArchetypesAssessed =
-    applicationArchetypes?.every(
-      (archetype) => archetype?.assessments?.length ?? 0 > 0
-    ) ?? false;
+    const allArchetypesAssessed =
+      assessmentsWithArchetypes.length > 0 &&
+      assessmentsWithArchetypes.every(({ archetype, assessments }) => {
+        const requiredAssessments = assessments.filter(
+          (assessment) => assessment.required
+        );
+        return (
+          archetype.assessed &&
+          assessments.length > 0 &&
+          requiredAssessments.length > 0 &&
+          requiredAssessments.every(
+            (assessment) => assessment.status === "complete"
+          )
+        );
+      });
+
+    const hasInProgressOrNotStartedRequiredAssessments =
+      assessmentsWithArchetypes.some(({ assessments }) =>
+        assessments.some(
+          (assessment) =>
+            assessment.required &&
+            ["empty", "started"].includes(assessment.status)
+        )
+      );
+
+    const assessedArchetypesWithARequiredAssessment =
+      assessmentsWithArchetypes.filter(({ assessments, archetype }) =>
+        assessments.some(
+          (assessment) =>
+            assessment.required &&
+            assessment.status === "complete" &&
+            archetype.assessed
+        )
+      );
+    const assessedArchetypeCount =
+      archetypes?.filter(
+        (archetype) =>
+          archetype?.assessments?.length ?? (0 > 0 && archetype.assessed)
+      ).length || 0;
+
+    return {
+      assessmentsWithArchetypes,
+      someArchetypesAssessed,
+      allArchetypesAssessed,
+      hasInProgressOrNotStartedRequiredAssessments,
+      assessedArchetypesWithARequiredAssessment,
+      assessedArchetypeCount,
+    };
+  }, [archetypes, assessments]);
 
   if (fetchError) {
     return <EmptyTextMessage message={t("terms.notAvailable")} />;
@@ -46,27 +95,24 @@ export const ApplicationAssessmentStatus: React.FC<
   let statusPreset: IconedStatusPreset = "NotStarted"; // Default status
   let tooltipCount: number = 0;
 
-  const assessedArchetypeCount =
-    applicationArchetypes?.filter(
-      (archetype) => archetype?.assessments?.length ?? 0 > 0
-    ).length || 0;
-
   const isDirectlyAssessed =
     application.assessed && (application.assessments?.length ?? 0) > 0;
+  const {
+    allArchetypesAssessed,
+    assessedArchetypesWithARequiredAssessment,
+    hasInProgressOrNotStartedRequiredAssessments,
+  } = assessmentStatusInfo;
 
   if (isDirectlyAssessed) {
     statusPreset = "Completed";
-  } else if (areAllArchetypesAssessed) {
+  } else if (allArchetypesAssessed) {
     statusPreset = "InheritedAssessments";
-    tooltipCount = assessedArchetypeCount;
-  } else if (someArchetypesAssessed) {
+    tooltipCount = assessedArchetypesWithARequiredAssessment.length;
+  } else if (hasInProgressOrNotStartedRequiredAssessments) {
     statusPreset = "InProgressInheritedAssessments";
-    tooltipCount = assessedArchetypeCount;
+    tooltipCount = assessedArchetypesWithARequiredAssessment.length;
   } else if (
-    filteredAssessments?.some(
-      (assessment) =>
-        assessment.status === "started" || assessment.status === "complete"
-    )
+    filteredAssessments?.some((assessment) => assessment.status === "started")
   ) {
     statusPreset = "InProgress";
   }
