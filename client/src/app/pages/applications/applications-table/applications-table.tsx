konveyor/tackle2-ui

commit e462612d53636a3197d7d876c2f9ccf21bc57ad4

:bug: Remove delete disable logic for applications assigned to wave (#1803)
Resolves https://issues.redhat.com/browse/MTA-2225

Signed-off-by: Ian Bolton <ibolton@redhat.com>

client/src/app/pages/applications/applications-table/applications-table.tsx
@@ -579,21 +579,14 @@ export const ApplicationsTable: React.FC = () => {
     : [];
   const applicationDropdownItems = applicationWriteAccess
     ? [
-        <ConditionalTooltip
-          key="delete-app-tooltip"
-          isTooltipEnabled={areAppsInWaves}
-          content={t("message.cannotDeleteApplicationsAssignedToMigrationWave")}
+        <DropdownItem
+          key="applications-bulk-delete"
+          onClick={() => {
+            setApplicationsToDelete(selectedRows);
+          }}
         >
-          <DropdownItem
-            key="applications-bulk-delete"
-            isAriaDisabled={areAppsInWaves || selectedRows.length < 1}
-            onClick={() => {
-              setApplicationsToDelete(selectedRows);
-            }}
-          >
-            {t("actions.delete")}
-          </DropdownItem>
-        </ConditionalTooltip>,
+          {t("actions.delete")}
+        </DropdownItem>,
         ...(credentialsReadAccess
           ? [
               <DropdownItem
@@ -1022,17 +1015,6 @@ export const ApplicationsTable: React.FC = () => {
                             ...(applicationWriteAccess
                               ? [
                                   {
-                                    isAriaDisabled:
-                                      application.migrationWave !== null,
-                                    tooltipProps: {
-                                      content:
-                                        application.migrationWave !== null
-                                          ? t(
-                                              "message.cannotDeleteApplicationsAssignedToMigrationWave"
-                                            )
-                                          : "",
-                                    },
-
                                     title: t("actions.delete"),
                                     onClick: () =>
                                       setApplicationsToDelete([application]),

commit 0501e1ce78f8883e6e099edbaa0e64d2ba402525

:bug: isDisabled logic regression (#1890)
Regressed in https://github.com/konveyor/tackle2-ui/pull/1803/files
Resolves https://github.com/konveyor/tackle2-ui/issues/1874

Signed-off-by: Ian Bolton <ibolton@redhat.com>

client/src/app/pages/applications/applications-table/applications-table.tsx
@@ -586,6 +586,7 @@ export const ApplicationsTable: React.FC = () => {
     ? [
         <DropdownItem
           key="applications-bulk-delete"
+          isDisabled={selectedRows.length < 1}
           onClick={() => {
             setApplicationsToDelete(selectedRows);
           }}