d12314be89a2ee6718298ee6667219e8b44325fc
Scott J Dickerson <sdickers@redhat.com>
2024-06-13 15:14:42 -0400

    drawer -- add empty state
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/components/task-manager/TaskManagerDrawer.tsx
+++ client/src/app/components/task-manager/TaskManagerDrawer.tsx
@@ -1,6 +1,13 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 import { Link } from "react-router-dom";
 import {
+  EmptyState,
+  EmptyStateActions,
+  EmptyStateBody,
+  EmptyStateFooter,
+  EmptyStateHeader,
+  EmptyStateIcon,
+  EmptyStateVariant,
   NotificationDrawer,
   NotificationDrawerBody,
   NotificationDrawerHeader,
@@ -9,8 +16,10 @@ import {
   NotificationDrawerListItemBody,
   NotificationDrawerListItemHeader,
 } from "@patternfly/react-core";
-import { useTaskManagerContext } from "./TaskManagerContext";
+import CubesIcon from "@patternfly/react-icons/dist/esm/icons/cubes-icon";
+
 import { Task } from "@app/api/models";
+import { useTaskManagerContext } from "./TaskManagerContext";
 
 interface TaskManagerDrawerProps {
   ref?: React.ForwardedRef<HTMLElement>;
@@ -19,6 +28,7 @@ interface TaskManagerDrawerProps {
 export const TaskManagerDrawer: React.FC<TaskManagerDrawerProps> = forwardRef(
   (_props, ref) => {
     const { isExpanded, setIsExpanded, queuedCount } = useTaskManagerContext();
+    const { tasks } = useTaskManagerData();
 
     const closeDrawer = () => {
       setIsExpanded(!isExpanded);
@@ -34,31 +44,37 @@ export const TaskManagerDrawer: React.FC<TaskManagerDrawerProps> = forwardRef(
           <Link to="/tasks">View All Tasks</Link>
         </NotificationDrawerHeader>
         <NotificationDrawerBody>
-          {/* TODO: add an empty state */}
-          <NotificationDrawerList>
-            <TaskItem
-              task={{
-                id: 999,
-                application: { id: 1 },
-                name: "Task 999",
-                addon: "analyzer",
-                data: {
-                  tagger: { enabled: true },
-                  verbosity: 0,
-                  mode: { artifact: "", binary: false, withDeps: false },
-                  scope: {
-                    withKnownLibs: false,
-                    packages: { included: [], excluded: [] },
-                  },
-                },
-              }}
-            />
-          </NotificationDrawerList>
+          {tasks.length == 0 ? (
+            <EmptyState variant={EmptyStateVariant.full}>
+              <EmptyStateHeader
+                headingLevel="h2"
+                titleText="There are no queued tasks"
+                icon={<EmptyStateIcon icon={CubesIcon} />}
+              />
+              <EmptyStateBody>
+                No tasks are currently ready, postponed, blocked, pending or
+                running. Completed and cancelled tasks may be viewed on the full
+                task list.
+              </EmptyStateBody>
+              <EmptyStateFooter>
+                <EmptyStateActions>
+                  <Link to="/tasks">View All Tasks</Link>
+                </EmptyStateActions>
+              </EmptyStateFooter>
+            </EmptyState>
+          ) : (
+            <NotificationDrawerList>
+              {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+              ))}
+            </NotificationDrawerList>
+          )}
         </NotificationDrawerBody>
       </NotificationDrawer>
     );
   }
 );
+TaskManagerDrawer.displayName = "TaskManagerDrawer";
 
 const TaskItem: React.FC<{ task: Task }> = ({ task }) => (
   <NotificationDrawerListItem key={task.id} variant="info">
@@ -71,4 +87,32 @@ const TaskItem: React.FC<{ task: Task }> = ({ task }) => (
   </NotificationDrawerListItem>
 );
 
-TaskManagerDrawer.displayName = "TaskManagerDrawer";
+const useTaskManagerData = () => {
+  const [pageSize, setPageSize] = useState(20);
+  const increasePageSize = () => {
+    setPageSize(pageSize + 20);
+  };
+
+  const tasks: Task[] = [
+    // {
+    //   id: 999,
+    //   application: { id: 1 },
+    //   name: "Task 999",
+    //   addon: "analyzer",
+    //   data: {
+    //     tagger: { enabled: true },
+    //     verbosity: 0,
+    //     mode: { artifact: "", binary: false, withDeps: false },
+    //     scope: {
+    //       withKnownLibs: false,
+    //       packages: { included: [], excluded: [] },
+    //     },
+    //   },
+    // },
+  ];
+
+  return {
+    tasks,
+    increasePageSize,
+  };
+};
