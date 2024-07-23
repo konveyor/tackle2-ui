6b038e355a77fcd0fc2fca83dd7785bb82f66e05
Scott Dickerson <sdickers@redhat.com>
2024-07-23 12:33:11 -0400

    :bug: Archetype table, applications column is now a link (#2024)
    When an archetype matches at least 1 application, render a link in the
    application columns that will navigate the user to the application table
    with a filter applied to show all of the applications matched to the
    archetype.
    
    The `Link` component is now shared between the archetype table and the
    archetype detail drawer.
    
    Resolves: https://issues.redhat.com/browse/MTA-1390
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

---
+++ client/src/app/pages/archetypes/components/link-to-archetype-applications.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { Text } from "@patternfly/react-core";
+
+import type { Archetype } from "@app/api/models";
+import { serializeFilterUrlParams } from "@app/hooks/table-controls";
+import { Paths } from "@app/Paths";
+
+const getApplicationsUrl = (archetypeName?: string) => {
+  if (!archetypeName) return "";
+
+  const filterValues = {
+    archetypes: [archetypeName],
+  };
+
+  const serializedParams = serializeFilterUrlParams(filterValues);
+
+  const queryString = serializedParams.filters
+    ? `filters=${serializedParams.filters}`
+    : "";
+  return `${Paths.applications}?${queryString}`;
+};
+
+const LinkToArchetypeApplications: React.FC<{
+  archetype: Archetype | null | undefined;
+  noApplicationsMessage?: React.ReactNode;
+}> = ({ archetype, noApplicationsMessage }) => {
+  const { t } = useTranslation();
+
+  const hasApplications = (archetype?.applications?.length ?? 0) > 0;
+
+  return !hasApplications && noApplicationsMessage ? (
+    <>{noApplicationsMessage}</>
+  ) : !hasApplications && !noApplicationsMessage ? (
+    <Text>{t("message.archetypeNoApplications")}</Text>
+  ) : (
+    <Link to={getApplicationsUrl(archetype?.name)}>
+      {t("message.archetypeApplicationCount", {
+        count: archetype?.applications?.length ?? 0,
+      })}
+    </Link>
+  );
+};
+
+export default LinkToArchetypeApplications;
