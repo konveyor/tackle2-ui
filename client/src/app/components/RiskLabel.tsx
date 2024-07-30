84fbbf7b1ceafef82556c8e9f4c2f4c35253da66
Scott J Dickerson <sdickers@redhat.com>
2024-07-30 14:40:38 -0400

    More type Risk work
      - Add "unassessed" as an office `Risk` type option
    
      - Updated `RiskLabel` to handle undefined and arbitrary
        strings as risk inputs.  Consumers of the component
        don't need to worry about the prop quite as much anymore.
    
      - Where a string risk was getting looked at, force to
        lower case first so all the constants match up.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/components/RiskLabel.tsx
+++ client/src/app/components/RiskLabel.tsx
@@ -7,15 +7,44 @@ import { RISK_LIST } from "@app/Constants";
 import { Risk } from "@app/api/models";
 
 export interface IRiskLabelProps {
-  risk: Risk;
+  risk?: Risk | string;
+}
+
+function normalizeToRisk(risk?: Risk | string): Risk | undefined {
+  let normal: Risk | undefined = undefined;
+
+  switch (risk) {
+    case "green":
+      normal = "green";
+      break;
+
+    case "yellow":
+      normal = "yellow";
+      break;
+
+    case "red":
+      normal = "red";
+      break;
+
+    case "unassessed":
+      normal = "unassessed";
+      break;
+
+    case "unknown":
+      normal = "unknown";
+      break;
+  }
+
+  return normal;
 }
 
 export const RiskLabel: React.FC<IRiskLabelProps> = ({
-  risk,
+  risk = "unknown",
 }: IRiskLabelProps) => {
   const { t } = useTranslation();
 
-  const data = RISK_LIST[risk];
+  const asRisk = normalizeToRisk(risk);
+  const data = !asRisk ? undefined : RISK_LIST[asRisk];
 
   return (
     <Label color={data ? data.labelColor : "grey"}>
