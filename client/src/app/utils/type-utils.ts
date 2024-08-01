0e579db57d4fb3037b5693d671e096b7be62dbe1
Scott Dickerson <sdickers@redhat.com>
2024-08-01 17:40:08 -0400

    :bug: [backport release-0.5] Add application risk filter of "Unassessed" (#2032) (#2035)
    Backport-of: #2032
    Resolves: https://issues.redhat.com/browse/MTA-2816
    
    Application risk options are one of:
      - High (red)
      - Medium (yellow)
      - Low (green)
      - Unknown (unknown)
      - Unassessed (unassessed)
    
    Added the __Unassessed__ option to the risk filter list on the
    application table to have a complete set of risk filters.
    
    Note: To help test risk levels, the questionnaire
    `hack/import-questionnaire/assign-risk.yaml` has been added. The single
    question has a 1 to 1 mapping between answer and risk level. As long as
    that questionnaire is the only one active on the system, it works well
    to quickly assign risks to applications.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/utils/type-utils.ts
+++ client/src/app/utils/type-utils.ts
@@ -1,3 +1,5 @@
+import { Risk } from "@app/api/models";
+
 export type KeyWithValueType<T, V> = {
   [Key in keyof T]-?: T[Key] extends V ? Key : never;
 }[keyof T];
@@ -10,3 +12,38 @@ export type DisallowCharacters<
 export type DiscriminatedArgs<TBoolDiscriminatorKey extends string, TArgs> =
   | ({ [key in TBoolDiscriminatorKey]: true } & TArgs)
   | { [key in TBoolDiscriminatorKey]?: false };
+
+/**
+ * Normalize an optional `Risk` or `string` input and return either the matching
+ * `Risk` or the `defaultValue` (which defaults to `undefined`).
+ */
+export function normalizeRisk(
+  risk?: Risk | string,
+  defaultValue?: Risk
+): Risk | undefined {
+  let normal: Risk | undefined = defaultValue;
+
+  switch (risk) {
+    case "green":
+      normal = "green";
+      break;
+
+    case "yellow":
+      normal = "yellow";
+      break;
+
+    case "red":
+      normal = "red";
+      break;
+
+    case "unassessed":
+      normal = "unassessed";
+      break;
+
+    case "unknown":
+      normal = "unknown";
+      break;
+  }
+
+  return normal;
+}
