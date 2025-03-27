48a696094eba46376d29723f872285f00aa564de
Scott J Dickerson <sdickers@redhat.com>
2025-03-27 18:34:34 -0400

    :bug: Update application asses override check and assessment column
    Fixes: https://issues.redhat.com/browse/MTA-1825
    
    On the application inventory page, update handling for the:
      - assessment column
      - assess an application action
    
    Assessment column:
      Collect the information about an application's assessment
      status as part of the `DecoratedApplication`.  This allows
      easy access to the direct and inherited assessments for
      an application.  The Assessment column component now uses
      this data for its rendering.
    
    Assess and application action:
      When invoking the "Assess" action on an application, check
      the assessment status:
        - If the application is already being directly assessed or
          does not belong to an archetype, push the user to the
          assessments page.
        - If the application belongs to an archetype, and if none
          of the archetypes have an assessment, push the user to
          the assessments page.
        - If the application's archetypes have any assessments,
          prompt the user if they want to override the archetype
          assessments or view the archetype's assessments.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/pages/applications/applications-table/applications-table.tsx
+++ client/src/app/pages/applications/applications-table/applications-table.tsx
@@ -76,7 +76,7 @@ import { checkAccess } from "@app/utils/rbac-utils";
 import { useLocalTableControls } from "@app/hooks/table-controls";
 
 // Queries
-import { getArchetypeById, getAssessmentsByItemId } from "@app/api/rest";
+import { getArchetypeById } from "@app/api/rest";
 import { Assessment, Ref, TaskState } from "@app/api/models";
 import {
   useBulkDeleteApplicationMutation,
@@ -117,7 +117,7 @@ import { ColumnApplicationName } from "./components/column-application-name";
 import {
   DecoratedApplication,
   useDecoratedApplications,
-} from "./useDecoratedApplications";
+} from "../useDecoratedApplications";
 import { useBulkSelection } from "@app/hooks/selection/useBulkSelection";
 
 export const ApplicationsTable: React.FC = () => {
@@ -126,6 +126,7 @@ export const ApplicationsTable: React.FC = () => {
 
   const history = useHistory();
   const token = keycloak.tokenParsed;
+
   // ----- State for the modals
   const [saveApplicationModalState, setSaveApplicationModalState] = useState<
     "create" | DecoratedApplication | null
@@ -742,33 +743,17 @@ export const ApplicationsTable: React.FC = () => {
   const assessSelectedApp = async (application: DecoratedApplication) => {
     setApplicationToAssess(application);
 
-    if (application?.archetypes?.length) {
-      for (const archetypeRef of application.archetypes) {
-        try {
-          const assessments = await getAssessmentsByItemId(
-            true,
-            archetypeRef.id
-          );
-
-          if (assessments && assessments.length > 0) {
-            setArchetypeRefsToOverride(application.archetypes);
-            break;
-          } else {
-            handleNavToAssessment(application);
-          }
-        } catch (error) {
-          console.error(
-            `Error fetching archetype with ID ${archetypeRef.id}:`,
-            error
-          );
-          pushNotification({
-            title: t("terms.error"),
-            variant: "danger",
-          });
-        }
-      }
-    } else {
+    const archetypes = application.archetypes ?? [];
+    const { directStatus, inheritedStatus } = application.assessmentStatus;
+    if (
+      archetypes.length === 0 ||
+      directStatus === "partial" ||
+      directStatus === "complete" ||
+      inheritedStatus === "none"
+    ) {
       handleNavToAssessment(application);
+    } else {
+      setArchetypeRefsToOverride(archetypes);
     }
   };
 
@@ -1004,7 +989,6 @@ export const ApplicationsTable: React.FC = () => {
                       >
                         <ApplicationAssessmentStatus
                           application={application}
-                          isLoading={isFetchingApplications}
                           key={`${application?.id}-assessment-status`}
                         />
                       </Td>
