12d34b37e8f1f6ed5a699077cdd2ca4394d26452
Scott Dickerson <sdickers@redhat.com>
2025-04-03 12:52:37 -0400

    :bug: Add "select all filtered" to bulk selection MTA-4886 (#2209)
    Resolves: https://issues.redhat.com/browse/MTA-4886
    
    Update the bulk selection hook and component to enable selecting:
      - nothing
      - the current page of items
      - the current set of filtered items (if provided)
      - the full set of items (if provided)
    
    Refactor the `getLocalTableControlDerviedState()` function into a react
    hook, and apply a memo to each derived state function call. This will
    keep the item references stable and allow bulk selection to function
    properly.
    
    The application inventory table passes in enough items such that the
    bulk selector can provide selection of: none, page, all filtered.
    
    ---------
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>
    Signed-off-by: Cherry Picker <noreply@github.com>

--- client/src/app/pages/applications/applications-table/applications-table.tsx
+++ client/src/app/pages/applications/applications-table/applications-table.tsx
@@ -507,8 +507,8 @@ export const ApplicationsTable: React.FC = () => {
   });
 
   const {
+    filteredItems,
     currentPageItems,
-    totalItemCount,
     numRenderedColumns,
     propHelpers: {
       toolbarProps,
@@ -529,10 +529,11 @@ export const ApplicationsTable: React.FC = () => {
     selectedItems: selectedRows,
     propHelpers: { toolbarBulkSelectorProps, getSelectCheckboxTdProps },
   } = useBulkSelection({
-    items: currentPageItems,
     isEqual: (a, b) => a.id === b.id,
+    // TODO: Pass `items` to also enable "select all items" if needed
+    // items: applications,
+    filteredItems,
     currentPageItems,
-    totalItemCount,
   });
 
   const clearFilters = () => {
