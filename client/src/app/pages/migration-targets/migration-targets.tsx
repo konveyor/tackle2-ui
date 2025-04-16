ad06ff4c7b358d1b3b29f503a524df336297f23f
Scott J Dickerson <sdickers@redhat.com>
2025-04-16 12:36:52 -0400

    :sparkles: Update custom migration targets filtering/creation
    Resolves: #2176
    
    On the custom migrations target page, update the page
    to better support provider types. Summary of changes:
      - Add support for target filtering by Name and Provider
      - The filter provider list is pulled from the
        existing targets' providers so all targets are all
        always visible to filtering
      - The provider type is now selected on the create modal
      - The provider type can be edited for any custom
        target
      - The list of providers is contained within the
        hook `useMigrationProviderList()`.  This allows
        the list to be changed with little disruption to
        other components
    
    Other related changes:
      - Resolved linting warnings
      - Fixed types for Target queries
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/pages/migration-targets/migration-targets.tsx
+++ client/src/app/pages/migration-targets/migration-targets.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import {
   DndContext,
   closestCenter,
@@ -7,6 +7,8 @@ import {
   useSensor,
   useSensors,
   DragOverlay,
+  DragStartEvent,
+  DragOverEvent,
 } from "@dnd-kit/core";
 import {
   arrayMove,
@@ -17,12 +19,14 @@ import { SortableItem } from "./components/dnd/sortable-item";
 import {
   PageSection,
   PageSectionVariants,
-  Grid,
-  GridItem,
   TextContent,
   Button,
   Text,
   Modal,
+  Toolbar,
+  ToolbarContent,
+  ToolbarGroup,
+  ToolbarItem,
 } from "@patternfly/react-core";
 import spacing from "@patternfly/react-styles/css/utilities/Spacing/spacing";
 import { useTranslation } from "react-i18next";
@@ -35,13 +39,19 @@ import { getAxiosErrorMessage } from "@app/utils/utils";
 import { CustomTargetForm } from "./components/custom-target-form";
 import { useSetting, useSettingMutation } from "@app/queries/settings";
 import { useDeleteTargetMutation, useFetchTargets } from "@app/queries/targets";
-import { ProviderType, Target } from "@app/api/models";
-import { SimpleSelect } from "@app/components/SimpleSelect";
+import { Target } from "@app/api/models";
+import { DEFAULT_PROVIDER } from "./useMigrationProviderList";
+import { useLocalTableControls } from "@app/hooks/table-controls";
+import {
+  FilterToolbar,
+  FilterType,
+  FilterValue,
+} from "@app/components/FilterToolbar";
+import { unique } from "radash";
 
 export const MigrationTargets: React.FC = () => {
   const { t } = useTranslation();
   const { pushNotification } = React.useContext(NotificationsContext);
-  const [provider, setProvider] = useState<ProviderType>("Java");
 
   const { targets, refetch: refetchTargets } = useFetchTargets();
 
@@ -62,7 +72,7 @@ export const MigrationTargets: React.FC = () => {
     targetsEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-  const [activeId, setActiveId] = useState(null);
+  const [activeId, setActiveId] = useState<number | null>(null);
 
   const onDeleteTargetSuccess = (target: Target, id: number) => {
     pushNotification({
@@ -124,22 +134,39 @@ export const MigrationTargets: React.FC = () => {
           response.data.id,
         ]);
       }
-    }
 
+      // Make sure the new target's provider is part of the provider's filter so it can be seen
+      if (filterState.filterValues["provider"]) {
+        let newProviderFilter: FilterValue = null;
+
+        const targetProvider = response.data.provider;
+        if (targetProvider) {
+          const fv = filterState.filterValues["provider"];
+          newProviderFilter = fv.includes(targetProvider)
+            ? fv
+            : [...fv, targetProvider];
+        }
+
+        filterState.setFilterValues({
+          name: filterState.filterValues["name"],
+          provider: newProviderFilter,
+        });
+      }
+    }
     setCreateUpdateModalState(null);
     refetchTargets();
   };
 
   const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor));
 
-  function handleDragOver(event: any) {
+  function handleDragOver(event: DragOverEvent) {
     if (targetOrderSetting.isSuccess) {
       const { active, over } = event;
 
-      if (active.id !== over.id) {
+      if (over && active.id !== over.id) {
         const reorderTarget = (items: number[]) => {
-          const oldIndex = items.indexOf(active.id);
-          const newIndex = items.indexOf(over.id);
+          const oldIndex = items.indexOf(active.id as number);
+          const newIndex = items.indexOf(over.id as number);
 
           return arrayMove(items, oldIndex, newIndex);
         };
@@ -151,74 +178,95 @@ export const MigrationTargets: React.FC = () => {
     }
   }
 
-  const handleDragStart = (event: any) => {
+  const handleDragStart = (event: DragStartEvent) => {
     const { active } = event;
-    setActiveId(active.id);
+    setActiveId(active.id as number);
   };
+
+  const tableControls = useLocalTableControls({
+    tableName: "target-cards",
+    items: targets,
+    idProperty: "name",
+    initialFilterValues: { provider: [DEFAULT_PROVIDER] },
+    columnNames: {
+      name: "name",
+      provider: "provider",
+    },
+    isFilterEnabled: true,
+    isPaginationEnabled: false,
+    isSortEnabled: false,
+    // TODO: Add `persistTo` handling if needed.
+    filterCategories: [
+      {
+        selectOptions: unique(
+          targets.map((target) => target.provider).filter(Boolean)
+        ).map((target) => ({ value: target })),
+        placeholderText: "Filter by language...",
+        categoryKey: "provider",
+        title: "Languages",
+        type: FilterType.multiselect,
+        matcher: (filter, target) => !!target.provider?.includes(filter),
+      },
+      {
+        placeholderText: "Filter by name...",
+        categoryKey: "name",
+        title: "Name",
+        type: FilterType.search,
+        matcher: (filter, target) =>
+          !!target.name?.toLowerCase().includes(filter.toLowerCase()),
+      },
+    ],
+  });
+
+  const {
+    filterState,
+    currentPageItems: filteredTargets,
+    propHelpers: { toolbarProps, filterToolbarProps },
+  } = tableControls;
+
+  const filteredTargetsInOrder = useMemo(() => {
+    if (!targetOrderSetting.isSuccess) {
+      return [];
+    }
+
+    return targetOrderSetting.data
+      .map((id) => filteredTargets.find((target) => target.id === id))
+      .filter(Boolean);
+  }, [filteredTargets, targetOrderSetting.data, targetOrderSetting.isSuccess]);
 
   return (
     <>
       <PageSection variant={PageSectionVariants.light}>
-        <Grid>
-          <GridItem span={10}>
-            <TextContent>
-              <Text component="h1">{t("terms.customTargets")}</Text>
-            </TextContent>
-          </GridItem>
-          <GridItem span={2}></GridItem>
-          <GridItem span={12}>
-            <TextContent>
-              <Text>{t("terms.customTargetsDetails")}</Text>
-            </TextContent>
-          </GridItem>
-          <GridItem span={2} className={spacing.mtSm}>
-            <SimpleSelect
-              variant="typeahead"
-              id="action-select"
-              toggleId="action-select-toggle"
-              toggleAriaLabel="Action select dropdown toggle"
-              aria-label={"Select provider"}
-              value={provider}
-              options={["Java", "Go"]}
-              onChange={(selection) => {
-                setProvider(selection as ProviderType);
-              }}
-            />
-          </GridItem>
-          <GridItem span={2} className={spacing.mtSm}>
-            <Button
-              id="create-target"
-              isInline
-              className={spacing.mlMd}
-              onClick={() => setCreateUpdateModalState("create")}
-            >
-              Create new
-            </Button>
-          </GridItem>
-        </Grid>
+        <TextContent>
+          <Text component="h1">{t("terms.customTargets")}</Text>
+        </TextContent>
+        <TextContent>
+          <Text>{t("terms.customTargetsDetails")}</Text>
+        </TextContent>
       </PageSection>
+
       <PageSection>
-        <Modal
-          id="create-edit-custom-tarrget-modal"
-          title={t(
-            targetToUpdate ? "dialog.title.update" : "dialog.title.new",
-            {
-              what: `${t("terms.customTargetOfType", {
-                type: provider,
-              })}`,
-            }
-          )}
-          variant="medium"
-          isOpen={isCreateUpdateModalOpen}
-          onClose={() => setCreateUpdateModalState(null)}
+        <Toolbar
+          {...toolbarProps}
+          clearAllFilters={() => filterToolbarProps.setFilterValues({})}
         >
-          <CustomTargetForm
-            providerType={provider}
-            target={targetToUpdate}
-            onSaved={onCustomTargetModalSaved}
-            onCancel={() => setCreateUpdateModalState(null)}
-          />
-        </Modal>
+          <ToolbarContent>
+            <FilterToolbar {...filterToolbarProps} breakpoint="md" />
+            <ToolbarGroup variant="button-group">
+              <ToolbarItem>
+                <Button
+                  id="create-target"
+                  isInline
+                  className={spacing.mlMd}
+                  onClick={() => setCreateUpdateModalState("create")}
+                >
+                  Create new
+                </Button>
+              </ToolbarItem>
+            </ToolbarGroup>
+          </ToolbarContent>
+        </Toolbar>
+
         <DndContext
           sensors={sensors}
           collisionDetection={closestCenter}
@@ -230,38 +278,19 @@ export const MigrationTargets: React.FC = () => {
             strategy={rectSortingStrategy}
           >
             <DndGrid>
-              {targetOrderSetting.isSuccess &&
-                targetOrderSetting.data.map((id) => {
-                  const matchingTarget = targets.find(
-                    (target) => target.id === id
-                  );
-                  if (
-                    matchingTarget &&
-                    matchingTarget.provider?.includes(provider)
-                  ) {
-                    return (
-                      <SortableItem
-                        key={id}
-                        id={id}
-                        onEdit={() => {
-                          if (matchingTarget) {
-                            setCreateUpdateModalState(matchingTarget);
-                          }
-                        }}
-                        onDelete={() => {
-                          const matchingTarget = targets.find(
-                            (target) => target.id === id
-                          );
-                          if (matchingTarget?.id) {
-                            deleteTarget(matchingTarget.id);
-                          }
-                        }}
-                      />
-                    );
-                  } else {
-                    return null;
-                  }
-                })}
+              {filteredTargetsInOrder.map((target) => (
+                <SortableItem
+                  key={target.id}
+                  id={target.id}
+                  onEdit={() => {
+                    setCreateUpdateModalState(target);
+                  }}
+                  onDelete={() => {
+                    // TODO: Add a delete confirmation modal.
+                    deleteTarget(target.id);
+                  }}
+                />
+              ))}
               <div ref={targetsEndRef} />
             </DndGrid>
             <DragOverlay>
@@ -270,6 +299,22 @@ export const MigrationTargets: React.FC = () => {
           </SortableContext>
         </DndContext>
       </PageSection>
+
+      <Modal
+        id="create-edit-custom-target-modal"
+        title={t(targetToUpdate ? "dialog.title.update" : "dialog.title.new", {
+          what: `${t("terms.customTarget")}`,
+        })}
+        variant="medium"
+        isOpen={isCreateUpdateModalOpen}
+        onClose={() => setCreateUpdateModalState(null)}
+      >
+        <CustomTargetForm
+          target={targetToUpdate}
+          onSaved={onCustomTargetModalSaved}
+          onCancel={() => setCreateUpdateModalState(null)}
+        />
+      </Modal>
     </>
   );
 };
