c099bae1937bab6f4bbbcd97b8ac3397c446a6fc
Scott Dickerson <sdickers@redhat.com>
2025-04-23 11:19:35 -0400

    :sparkles: Update custom migration targets filtering/creation (#2230)
    Resolves: #2176
    Resolves: https://issues.redhat.com/browse/MTA-4990
    
    On the custom migrations target page, update the page to better support
    provider types. Summary of changes:
      - Add support for target filtering by Name and Provider
      - The filter provider list is pulled from the existing targets'
        providers so all targets are all always visible to filtering
      - The provider type is now selected on the create modal
      - The provider type can be edited for any custom target
      - The list of providers is contained within the hook
        `useMigrationProviderList()`. This allows the list to be changed with
        little disruption to other components
    
    New list of provider types (names chosen based on the Language tag that
    will activate their respective analyzers):
      - Java
      - Golang
      - Python
      - TypeScript
    
    Other related changes:
      - Resolved linting warnings
      - Fixed types for Target queries
    
    ---------
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

---
+++ client/src/app/pages/migration-targets/useMigrationProviderList.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_PROVIDER = "Java";
+
+const PROVIDER_LIST = ["Java", "Golang", "TypeScript", "Python"].sort();
+
+/**
+ * Provides the list of migration provider names that can be used for custom targets. These names
+ * do not need to match any other names, they are not used in hub. This list is just for UI
+ * filtering and sorting.
+ */
+export const useMigrationProviderList = (): string[] => {
+  return PROVIDER_LIST;
+};
