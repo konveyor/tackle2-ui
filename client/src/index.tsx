e8e03348ab12caef52393bcd11e3f88af62bdc5f
Scott Dickerson <sdickers@redhat.com>
2025-05-21 14:02:57 -0400

    :ghost: remove web-vitals (#2318)
    Remove the use of the web-vitals package. We had it linked but never
    reported or used. If we need it in the future, it is easy enough to add
    back. Removing it will reduce dependency exposure.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,6 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import ENV from "@app/env";
 import App from "@app/App";
-import reportWebVitals from "@app/reportWebVitals";
 import { KeycloakProvider } from "@app/components/KeycloakProvider";
 
 import "@app/dayjs";
@@ -38,8 +37,3 @@ if (ENV.NODE_ENV === "development") {
 } else {
   renderApp();
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
