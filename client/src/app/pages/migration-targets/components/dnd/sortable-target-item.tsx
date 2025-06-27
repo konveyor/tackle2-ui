46ae818ea639d2e05fca8e6ab1717da71226af1c
Scott Dickerson <sdickers@redhat.com>
2025-06-27 15:50:59 -0400

    :sparkles: Upgrade @dnd-kit/sortable, refactor migration-targets (#2406)
    Upgrade `@dnd-kit` and `@dnd-kit/sortable` to latest.
    
    Refactor the migration-targets page and target card components.
    Simplified the components for more streamlined use on the custom
    migration targets page and on the analysis wizard target selection step.
    The migration-targets page layout updated to use `Gallery` instead of
    the custom grid.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

---
+++ client/src/app/pages/migration-targets/components/dnd/sortable-target-item.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { Target } from "@app/api/models";
+import { TargetItem } from "./target-item";
+
+interface SortableTargetItemProps {
+  style?: React.CSSProperties;
+  target: Target;
+  onEdit?: () => void;
+  onDelete?: () => void;
+}
+
+export const SortableTargetItem: React.FC<SortableTargetItemProps> = ({
+  style,
+  target,
+  onEdit,
+  onDelete,
+}) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    setActivatorNodeRef,
+  } = useSortable({ id: target.id });
+
+  const itemStyles = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    ...style,
+  } as React.CSSProperties;
+
+  return (
+    <TargetItem
+      ref={setNodeRef}
+      style={itemStyles}
+      onEdit={onEdit}
+      onDelete={onDelete}
+      target={target}
+      activatorNodeRef={setActivatorNodeRef}
+      {...attributes}
+      {...listeners}
+    />
+  );
+};
