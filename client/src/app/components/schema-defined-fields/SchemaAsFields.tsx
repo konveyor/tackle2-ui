9743100acd7b85f5ec7622af56605d462899ef66
Scott J Dickerson <sdickers@redhat.com>
2025-07-16 22:37:30 -0400

    refactor SchemaAsFields
      - use render components,
      - update fields when the jsonDocument changes,
      - subscribe to form changes differently,
      - in nested object renders, make sure the field name
        is setup to be the nested name so setValue updates
        the change in the right place
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/components/schema-defined-fields/SchemaAsFields.tsx
+++ client/src/app/components/schema-defined-fields/SchemaAsFields.tsx
@@ -1,15 +1,13 @@
 /* eslint-disable no-case-declarations */
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider, useFormContext } from "react-hook-form";
 
 import {
-  Card,
-  CardBody,
-  CardTitle,
   Checkbox,
   Form,
-  Title,
+  FormFieldGroup,
+  FormFieldGroupHeader,
 } from "@patternfly/react-core";
 
 import {
@@ -31,6 +29,201 @@ export interface SchemaAsFieldsProps {
   isReadOnly?: boolean;
 }
 
+interface FieldProps {
+  isReadOnly?: boolean;
+  fieldKey: string;
+  fieldSchema: JsonSchemaObject;
+  path: string;
+  required?: boolean;
+}
+
+const StringField: React.FC<FieldProps> = ({
+  isReadOnly,
+  fieldKey,
+  fieldSchema,
+  path,
+  required,
+}) => {
+  const { control } = useFormContext();
+  const fieldId = `field-${path}`;
+
+  return (
+    <HookFormPFTextInput
+      isDisabled={isReadOnly}
+      control={control}
+      name={path}
+      label={fieldSchema.title || fieldKey}
+      helperText={fieldSchema.description}
+      fieldId={fieldId}
+      isRequired={required}
+    />
+  );
+};
+
+const NumberField: React.FC<FieldProps> = ({
+  isReadOnly,
+  fieldKey,
+  fieldSchema,
+  path,
+  required,
+}) => {
+  const { control } = useFormContext();
+  const fieldId = `field-${path}`;
+
+  return (
+    <HookFormPFTextInput
+      isDisabled={isReadOnly}
+      control={control}
+      name={path}
+      label={fieldSchema.title || fieldKey}
+      helperText={fieldSchema.description}
+      fieldId={fieldId}
+      isRequired={required}
+      type={fieldSchema.type === "integer" ? "integer" : "number"}
+    />
+  );
+};
+
+const BooleanField: React.FC<FieldProps> = ({
+  isReadOnly,
+  fieldKey,
+  fieldSchema,
+  path,
+  required,
+}) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+  const fieldId = `field-${path}`;
+  const errorMessage = errors[path]?.message as string;
+  const isValid = !errorMessage;
+
+  return (
+    <HookFormPFGroupController
+      control={control}
+      name={path}
+      label={fieldSchema.title || fieldKey}
+      helperText={fieldSchema.description}
+      fieldId={fieldId}
+      isRequired={required}
+      renderInput={({ field: { value, onChange } }) => (
+        <>
+          <Checkbox
+            id={fieldId}
+            label={fieldSchema.title || fieldKey}
+            isDisabled={isReadOnly}
+            isChecked={!!value}
+            onChange={onChange}
+            aria-invalid={!isValid}
+            aria-describedby={errorMessage ? `${fieldId}-helper` : undefined}
+          />
+        </>
+      )}
+    />
+  );
+};
+
+const ObjectField: React.FC<FieldProps> = ({
+  isReadOnly,
+  fieldKey,
+  fieldSchema,
+  path,
+}) => {
+  return (
+    <FormFieldGroup
+      style={{
+        gridTemplateColumns: "2rem 1fr",
+      }}
+      header={
+        <FormFieldGroupHeader
+          titleText={{
+            text: fieldSchema.title || fieldKey,
+            id: `field-${path}-title`,
+          }}
+          titleDescription={fieldSchema.description}
+        />
+      }
+    >
+      {Object.keys(fieldSchema.properties || {}).map((nestedKey) => (
+        <SchemaField
+          isReadOnly={isReadOnly}
+          key={nestedKey}
+          fieldKey={nestedKey}
+          fieldSchema={fieldSchema.properties![nestedKey]}
+          path={path ? `${path}.${nestedKey}` : nestedKey}
+          parentSchema={fieldSchema}
+        />
+      ))}
+    </FormFieldGroup>
+  );
+};
+
+interface SchemaFieldProps {
+  isReadOnly?: boolean;
+  fieldKey: string;
+  fieldSchema: JsonSchemaObject;
+  path: string;
+  parentSchema?: JsonSchemaObject;
+}
+
+const SchemaField: React.FC<SchemaFieldProps> = ({
+  isReadOnly,
+  fieldKey,
+  fieldSchema,
+  path,
+  parentSchema,
+}) => {
+  const required =
+    parentSchema?.required && parentSchema.required.includes(fieldKey);
+
+  switch (fieldSchema.type) {
+    case "string":
+      return (
+        <StringField
+          isReadOnly={isReadOnly}
+          fieldKey={fieldKey}
+          fieldSchema={fieldSchema}
+          path={path}
+          required={required}
+        />
+      );
+    case "number":
+    case "integer":
+      return (
+        <NumberField
+          isReadOnly={isReadOnly}
+          fieldKey={fieldKey}
+          fieldSchema={fieldSchema}
+          path={path}
+          required={required}
+        />
+      );
+    case "boolean":
+      return (
+        <BooleanField
+          isReadOnly={isReadOnly}
+          fieldKey={fieldKey}
+          fieldSchema={fieldSchema}
+          path={path}
+          required={required}
+        />
+      );
+    case "object":
+      return (
+        <ObjectField
+          isReadOnly={isReadOnly}
+          fieldKey={fieldKey}
+          fieldSchema={fieldSchema}
+          path={path}
+          required={required}
+        />
+      );
+    default:
+      return null; // TODO: type="array" is not supported yet
+  }
+};
+
 export const SchemaAsFields: React.FC<SchemaAsFieldsProps> = ({
   jsonDocument,
   jsonSchema,
@@ -40,114 +233,53 @@ export const SchemaAsFields: React.FC<SchemaAsFieldsProps> = ({
 }) => {
   const { t } = useTranslation();
 
-  const handleFormChange = React.useCallback(
-    (values: JsonSchemaValues) => {
-      if (onDocumentChanged) {
-        onDocumentChanged(values);
-      }
-    },
-    [onDocumentChanged]
-  );
-
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-    watch,
-  } = useForm<JsonSchemaValues>({
+  const methods = useForm<JsonSchemaValues>({
     resolver: jsonSchemaToYupResolver(jsonSchema, t),
     defaultValues: jsonDocument,
     mode: "all",
   });
 
+  const { handleSubmit, reset, subscribe } = methods;
+
+  // Update form values when jsonDocument prop changes
   React.useEffect(() => {
-    const subscription = watch((values) => {
-      handleFormChange(values as JsonSchemaValues);
+    reset(jsonDocument);
+  }, [jsonDocument, reset]);
+
+  // Call onDocumentChanged when the form values change
+  React.useEffect(() => {
+    const subscription = subscribe({
+      formState: {
+        values: true,
+        isValid: true,
+      },
+      callback: ({ isValid, values }) => {
+        if (isValid) onDocumentChanged?.(values);
+      },
     });
-    return () => subscription.unsubscribe();
-  }, [watch, handleFormChange]);
-
-  const renderField = (key: string, fieldSchema: any) => {
-    const required = jsonSchema.required && jsonSchema.required.includes(key);
-    const fieldId = `field-${key}`;
-    const errorMessage = errors[key]?.message as string;
-    const isValid = !errorMessage;
-
-    switch (fieldSchema.type) {
-      case "string":
-      case "number":
-      case "integer":
-        return (
-          <HookFormPFTextInput
-            control={control}
-            name={key}
-            label={key}
-            fieldId={fieldId}
-            isRequired={required}
-          />
-        );
-      case "boolean":
-        return (
-          <HookFormPFGroupController
-            control={control}
-            name={key}
-            label={key}
-            fieldId={fieldId}
-            isRequired={required}
-            renderInput={({ field: { value, onChange } }) => (
-              <>
-                <Checkbox
-                  id={fieldId}
-                  label={key}
-                  isChecked={!!value}
-                  onChange={onChange}
-                  aria-invalid={!isValid}
-                  aria-describedby={
-                    errorMessage ? `${fieldId}-helper` : undefined
-                  }
-                />
-              </>
-            )}
-          />
-        );
-      case "object":
-        // Render nested objects recursively
-        return (
-          <Card
-            key={key}
-            isPlain
-            isCompact
-            style={{
-              marginTop: "20px",
-              border: "1px solid var(--pf-v5-global--BorderColor--100)",
-              borderRadius: "4px",
-            }}
-          >
-            <CardTitle>
-              <Title headingLevel="h3" size="lg">
-                {fieldSchema.title || key}
-              </Title>
-            </CardTitle>
-            <CardBody>
-              {Object.keys(fieldSchema.properties || {}).map((nestedKey) =>
-                renderField(nestedKey, fieldSchema.properties[nestedKey])
-              )}
-            </CardBody>
-          </Card>
-        );
-    }
-  };
+    return () => subscription();
+  }, [subscribe, onDocumentChanged]);
 
   const onValidSubmit = (values: JsonSchemaValues) => {
-    onDocumentSaved && onDocumentSaved(values);
+    onDocumentSaved?.(values);
   };
 
   return (
-    <Form onSubmit={handleSubmit(onValidSubmit)} id="schema-as-fields-form">
-      {Object.keys(jsonSchema?.properties || {}).map(
-        (key) =>
-          jsonSchema.properties && renderField(key, jsonSchema.properties[key])
-      )}
-    </Form>
+    <FormProvider {...methods}>
+      <Form onSubmit={handleSubmit(onValidSubmit)} id="schema-as-fields-form">
+        {Object.keys(jsonSchema?.properties || {}).map((key) =>
+          jsonSchema.properties ? (
+            <SchemaField
+              isReadOnly={isReadOnly}
+              key={key}
+              fieldKey={key}
+              fieldSchema={jsonSchema.properties[key]}
+              path={key}
+              parentSchema={jsonSchema}
+            />
+          ) : null
+        )}
+      </Form>
+    </FormProvider>
   );
 };
