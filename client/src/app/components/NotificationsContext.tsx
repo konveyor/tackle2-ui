c1e32e7bf423b1c9e139bd2c9468e127190f7d1d
Scott J Dickerson <sdickers@redhat.com>
2025-07-26 18:35:07 -0400

    Add useNotifications() hook
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/components/NotificationsContext.tsx
+++ client/src/app/components/NotificationsContext.tsx
@@ -32,6 +32,16 @@ export const NotificationsContext = React.createContext<INotificationsContext>(
   appContextDefaultValue
 );
 
+export const useNotifications = () => {
+  const context = React.useContext(NotificationsContext);
+  if (!context) {
+    throw new Error(
+      "useNotifications must be used within a NotificationsProvider"
+    );
+  }
+  return context;
+};
+
 export const NotificationsProvider: React.FunctionComponent<
   INotificationsProvider
 > = ({ children }: INotificationsProvider) => {
