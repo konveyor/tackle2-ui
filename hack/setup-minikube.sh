e7ae29d1a0da4d69caac444579ae82b62cebc9be
Scott J Dickerson <sdickers@redhat.com>
2025-07-28 17:39:41 -0400

    :ghost: Add setup-minikube.sh following docs
    Changes:
      - Add `setup-minikube.sh` in a way that follows `docs/local-minikube-setup.md`
      - Update `setup-operator.sh` with some more verbose logging
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

---
+++ hack/setup-minikube.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eo pipefail
+
+if ! command -v minikube >/dev/null 2>&1; then
+  echo "minikube not found, please install it:"
+  echo "    https://minikube.sigs.k8s.io/docs/start/"
+  exit 1
+fi
+
+# Configure minikube with our preferred settings for running on Fedora and start
+set_if_not_set() {
+  if ! minikube config get $1 >/dev/null 2>&1; then
+    echo "Setting $1 to $2"
+    minikube config set $1 $2
+  fi
+}
+set_if_not_set driver kvm2
+set_if_not_set memory 28000
+set_if_not_set cpus 4
+set_if_not_set disk-size 35g
+
+minikube start --addons=dashboard --addons=ingress
+
+# Install OLM
+OLM_VERSION="v0.32.0"
+curl \
+  -L https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${OLM_VERSION}/install.sh \
+  -o install_${OLM_VERSION}.sh
+
+chmod +x install_${OLM_VERSION}.sh
+./install_${OLM_VERSION}.sh ${OLM_VERSION}
+rm ./install_${OLM_VERSION}.sh
