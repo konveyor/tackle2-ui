749ce4e4368d27f78223ed0027c59185b0eee1d4
Scott J Dickerson <sdickers@redhat.com>
2025-08-29 18:17:18 -0400

    :seedling: Enable import sorting via eslint-plugin-import
    File import ordering will now be enforced via eslint
    rule.  The rule is set to `warn`, and any file that
    is added to a commit will have its import order rearranged
    as necessary through a lint-staged precommit hook.
    
    Import order:
      - 3rd party libraries first (React, axios, etc.)
      - PatternFly libraries second (@patternfly/**)
      - @app imports third (@app/**)
      - Relative imports last (../, ./)
    
    Within each group, import lines will be alpha sorted
    by import from name.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- server/rollup.config.js
+++ server/rollup.config.js
@@ -1,8 +1,8 @@
 /* eslint-env node */
 
+import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import run from "@rollup/plugin-run";
 
 const buildAndRun = process.env?.ROLLUP_RUN === "true";
