579373bee6a91ec9280de781d3b76d7554cd4aa8
Shveta Sachdeva <sshveta@redhat.com>
2025-10-17 14:04:13 -0700

    :test_tube:  First PR to move UI tests (#2656)
    Resolves: #2668
    
    Setup workspace `cypress` that will hold all of the e2e cypress tests.
    The baseline login test is included to make sure everything runs
    properly. Some libraries in use have been updated.
    
    Related change: To make sure jest, currently only used by the client
    workspace, does not conflict with cypress, the jest devDependencies were
    moved to `client/pacakge.json`.
    
    ## Summary by CodeRabbit
    
    * **New Features**
      * Added end-to-end Cypress framework: initial login test, test helpers,
        UI selectors, custom commands, reporters, and utilities for downloads
        and tagging.
    
    * **Documentation**
      * Added comprehensive Cypress README and an example environment file.
    
    * **Chores**
      * CI change-detection updated to include Cypress files.
      * Added Cypress workspace, scripts, lint/format configs and tooling.
      * Husky install now skips in CI/production.
      * VCS and formatting ignores updated; testing deps added to client.
    
    ---------
    
    Signed-off-by: Shveta Sachdeva <sshveta@redhat.com>
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>
    Co-authored-by: Scott J Dickerson <sdickers@redhat.com>
    Signed-off-by: Cherry Picker <noreply@github.com>

---
+++ cypress/.eslintrc.cjs
@@ -0,0 +1,10 @@
+/* eslint-env node */
+
+module.exports = {
+  root: false,
+
+  rules: {
+    "@typescript-eslint/no-var-requires": "warn",
+    "@typescript-eslint/no-namespace": "warn",
+  },
+};
