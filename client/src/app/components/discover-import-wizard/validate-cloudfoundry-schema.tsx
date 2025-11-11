9dffc7ae8e9c47a4b11766a53cb755dd08c7f797
Scott J Dickerson <sdickers@redhat.com>
2025-11-11 16:25:40 -0500

    :bug: Update Cloud Foundry discovery filter form
    Resolves: #2726
    Requires: https://github.com/konveyor/operator/pull/491
    
    Add `minItems: 1` for organizations filter prop to the Cloud
    Foundry discovery filter form.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/components/discover-import-wizard/validate-cloudfoundry-schema.tsx
+++ client/src/app/components/discover-import-wizard/validate-cloudfoundry-schema.tsx
@@ -15,27 +15,28 @@ const SUPPORTED_SCHEMA: JsonSchemaObject = {
   properties: {
     organizations: {
       description: "Organization names.",
-      items: {
-        minLength: 1,
-        type: "string",
-      },
       type: "array",
+      minItems: 1,
+      items: {
+        type: "string",
+        minLength: 1,
+      },
     },
     spaces: {
       description: "Space names.",
-      items: {
-        minLength: 1,
-        type: "string",
-      },
       type: "array",
+      items: {
+        type: "string",
+        minLength: 1,
+      },
     },
     names: {
       description: "Application names. Each may be a glob expression.",
-      items: {
-        minLength: 1,
-        type: "string",
-      },
       type: "array",
+      items: {
+        type: "string",
+        minLength: 1,
+      },
     },
   },
   type: "object",
