644aa7688e4177fc6f2d8c1df1acc160bd5b2480
Scott J Dickerson <sdickers@redhat.com>
2025-11-13 15:45:22 -0500

    drop workspace lintstagedrc.mjs since they just repeated the same config as the root
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- common/.lintstagedrc.mjs
+++
@@ -1,6 +0,0 @@
-import { NOT_SOURCE_FILES, SOURCE_FILES } from "../.lintstagedrc.mjs";
-
-export default {
-  [SOURCE_FILES]: ["prettier --write", "eslint --fix"],
-  [NOT_SOURCE_FILES]: "prettier --ignore-unknown --write",
-};
