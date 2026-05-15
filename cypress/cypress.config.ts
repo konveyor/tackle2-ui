6cb402f1909e6e7ede914db0129970435a028391
Radoslaw Szwajkowski <rszwajko@redhat.com>
2026-05-15 17:58:26 +0200

    increase the browser viewport
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -127,6 +127,51 @@ export default defineConfig({
       cypressFastFail(on, config);
       cypressFsPlugins(on, config);
       cypressMochawesomeReporter(on, config);
+      on(
+        "before:browser:launch",
+        (
+          browser: Cypress.Browser,
+          launchOptions: Cypress.BeforeBrowserLaunchOptions
+        ) => {
+          console.log(
+            "launching browser %s is headless? %s",
+            browser.name,
+            browser.isHeadless
+          );
+
+          // the browser width and height we want to get
+          // our screenshots and videos will be of that resolution
+          const width = 1920;
+          const height = 1080;
+
+          console.log(
+            "setting the browser window size to %d x %d",
+            width,
+            height
+          );
+
+          if (browser.name === "chrome" && browser.isHeadless) {
+            launchOptions.args.push(`--window-size=${width},${height}`);
+
+            // force screen to be non-retina and just use our given resolution
+            launchOptions.args.push("--force-device-scale-factor=1");
+          }
+
+          if (browser.name === "electron" && browser.isHeadless) {
+            // might not work on CI for some reason
+            launchOptions.preferences.width = width;
+            launchOptions.preferences.height = height;
+          }
+
+          if (browser.name === "firefox" && browser.isHeadless) {
+            launchOptions.args.push(`--width=${width}`);
+            launchOptions.args.push(`--height=${height}`);
+          }
+
+          // IMPORTANT: return the updated browser launch options
+          return launchOptions;
+        }
+      );
 
       on("task", { downloadFile });
       on("task", verifyDownloadTasks);
