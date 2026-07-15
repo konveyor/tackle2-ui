d52c07f498d3dde005e6eb5103f5f655696fea8b
Scott J Dickerson <sdickers@redhat.com>
2026-07-15 13:41:14 -0400

    :seedling: Refactor application import rest functions to correct return types
    Depends on #3471
    Part of #2630
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/api/rest/imports.ts
+++ client/src/app/api/rest/imports.ts
@@ -8,36 +8,31 @@ const APP_IMPORTS_SUMMARY = hub`/importsummaries`;
 const APP_IMPORTS_SUMMARY_CSV = hub`/importsummaries/download`;
 export const APP_IMPORTS_SUMMARY_UPLOAD = hub`/importsummaries/upload`;
 
-export const getApplicationsImportSummary = (): Promise<
-  ApplicationImportSummary[]
-> => axios.get(APP_IMPORTS_SUMMARY).then((response) => response.data);
+export const getApplicationsImportSummary = () =>
+  axios
+    .get<ApplicationImportSummary[]>(APP_IMPORTS_SUMMARY)
+    .then((response) => response.data);
 
-export const getApplicationImportSummaryById = (
-  id: number | string
-): Promise<ApplicationImportSummary> =>
-  axios.get(`${APP_IMPORTS_SUMMARY}/${id}`).then((response) => response.data);
+export const getApplicationImportSummaryById = (id: number | string) =>
+  axios
+    .get<ApplicationImportSummary>(`${APP_IMPORTS_SUMMARY}/${id}`)
+    .then((response) => response.data);
 
-export const deleteApplicationImportSummary = (
-  id: number
-): Promise<ApplicationImportSummary> =>
-  axios.delete(`${APP_IMPORTS_SUMMARY}/${id}`);
+export const deleteApplicationImportSummary = (id: number) =>
+  axios.delete<void>(`${APP_IMPORTS_SUMMARY}/${id}`);
 
 export const getApplicationImports = (
   importSummaryID: number,
   isValid: boolean | string
-): Promise<ApplicationImport[]> =>
+) =>
   axios
-    .get(
-      `${APP_IMPORTS}?importSummary.id=${importSummaryID}&isValid=${isValid}`
-    )
+    .get<
+      ApplicationImport[]
+    >(`${APP_IMPORTS}?importSummary.id=${importSummaryID}&isValid=${isValid}`)
     .then((response) => response.data);
 
-export const getApplicationSummaryCSV = (id: string) => {
-  return axios.get<ArrayBuffer>(
-    `${APP_IMPORTS_SUMMARY_CSV}?importSummary.id=${id}`,
-    {
-      responseType: "arraybuffer",
-      headers: { Accept: "text/csv" },
-    }
-  );
-};
+export const getApplicationSummaryCSV = (id: string) =>
+  axios.get<ArrayBuffer>(`${APP_IMPORTS_SUMMARY_CSV}?importSummary.id=${id}`, {
+    responseType: "arraybuffer",
+    headers: { Accept: "text/csv" },
+  });
