9ce795689b02c76bfd67a4987a00e51487b62e9b
Scott J Dickerson <sdickers@redhat.com>
2026-07-15 13:41:15 -0400

    :seedling: Refactor facts rest function to correct return type
    Depends on #3471
    Part of #2630
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- client/src/app/api/rest/facts.ts
+++ client/src/app/api/rest/facts.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-import { UnstructuredFact } from "../models";
 import { hub } from "../rest";
 
 export const getFacts = (id: number | string | undefined) =>
-  // TODO: Address this when moving to structured facts api
   id
     ? axios
-        .get<UnstructuredFact>(hub`/applications/${id}/facts`)
+        .get<Record<string, unknown>>(hub`/applications/${id}/facts`)
         .then((response) => response.data)
     : Promise.reject();
